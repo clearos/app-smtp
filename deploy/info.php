63cf03bc81c0365529cfd188a268e999ca9ed804
pcbaldwin <pbaldwin@clearfoundation.com>
2015-01-16 15:35:59 -0500

    Changed subcategory

--- deploy/info.php
+++ deploy/info.php
@@ -5,7 +5,7 @@
 /////////////////////////////////////////////////////////////////////////////
 
 $app['basename'] = 'smtp';
-$app['version'] = '2.0.10';
+$app['version'] = '2.0.14';
 $app['release'] = '1';
 $app['vendor'] = 'ClearFoundation';
 $app['packager'] = 'ClearFoundation';
@@ -19,7 +19,7 @@ $app['description'] = lang('smtp_app_description');
 
 $app['name'] = lang('smtp_app_name');
 $app['category'] = lang('base_category_server');
-$app['subcategory'] = lang('base_subcategory_mail');
+$app['subcategory'] = lang('base_subcategory_messaging');
 
 /////////////////////////////////////////////////////////////////////////////
 // Controllers
--- packaging/app-smtp.spec
+++ packaging/app-smtp.spec
@@ -1,7 +1,7 @@
 
 Name: app-smtp
 Epoch: 1
-Version: 2.0.10
+Version: 2.0.14
 Release: 1%{dist}
 Summary: SMTP Server
 License: GPLv3
