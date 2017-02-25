945bb2f59fde41657a7fab06cd9fea3d2189c4f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mk/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Со цел да им овозможи на сите функции во SMTP сервер, системот на сметки мора да се иницијализира';
+$lang['smtp_app_description'] = 'SMTP серверот обезбедува појдовна и дојдовна пошта сервер, како и пошта товар и SMTP проверка функции.';
+$lang['smtp_app_name'] = 'SMTP сервер';
+$lang['smtp_block_plaintext'] = 'Блок обичен текст лозинки';
+$lang['smtp_configure_accounts'] = 'Конфигурирај профили';
+$lang['smtp_destination_domains'] = 'код на домени';
+$lang['smtp_failed_login_detection'] = 'SMTP неуспешни обиди за најава';
+$lang['smtp_mail_disclaimer_invalid'] = 'Пошта Општи услови не е валидна.';
+$lang['smtp_mail_forwarding'] = 'Препраќање на пошта';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Поставките на име и домен може да се конфигурира во стан Settings пошта.';
+$lang['smtp_maximum_message_size'] = 'Максимална големина на пораката';
+$lang['smtp_port_invalid'] = 'Порт е валиден.';
+$lang['smtp_recipient_mapping_invalid'] = 'Примачот мапирање не е валиден.';
+$lang['smtp_relay_host_invalid'] = 'Реле домаќин е валиден.';
+$lang['smtp_relay_host'] = 'реле Домаќинот';
+$lang['smtp_server_invalid'] = 'SMTP серверот не е валиден.';
+$lang['smtp_smtp_authentication'] = 'SMTP проверка на автентичност';
+$lang['smtp_smtp_server'] = 'SMTP сервер';
+$lang['smtp_target_server'] = 'цел сервер';
+$lang['smtp_trusted_network_invalid'] = 'Доверлива мрежа не е валиден.';
+$lang['smtp_trusted_networks'] = 'доверлив мрежи';
+$lang['smtp_user_policies'] = 'Политика на корисникот';
