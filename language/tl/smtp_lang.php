09357ad396db5864c63d9c425b05262b9d549157
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tl/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Upang paganahin ang lahat ng mga tampok sa SMTP Server, ang mga account ng sistema ay dapat na-initialize';
+$lang['smtp_app_description'] = 'Ang SMTP Server ay nagbibigay ng isang papasok at palabas na mail server pati na rin ang mail forwarding at SMTP authentication tampok.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Passwords';
+$lang['smtp_configure_accounts'] = 'I-configure ang Accounts';
+$lang['smtp_destination_domains'] = 'Destinasyon Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP Nabigong Pag-login';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer ay hindi wasto.';
+$lang['smtp_mail_forwarding'] = 'mail Pagpapasa';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Ang hostname at domain setting ay maaaring naka-configure sa Mga Setting Mail app.';
+$lang['smtp_maximum_message_size'] = 'Pinakamataas na Sukat ng Mensahe';
+$lang['smtp_port_invalid'] = 'Port ay hindi wasto.';
+$lang['smtp_recipient_mapping_invalid'] = 'Tatanggap mapping ay hindi wasto.';
+$lang['smtp_relay_host_invalid'] = 'Relay host ay hindi wasto.';
+$lang['smtp_relay_host'] = 'relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server ay hindi wasto.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'target Server';
+$lang['smtp_trusted_network_invalid'] = 'Trusted network ay hindi wasto.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'Mga Patakaran User';
