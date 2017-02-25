3c723df62e2cd2f448c997c697b085492b6acd42
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:12:52 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sq/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Për të mundësuar të gjitha tiparet në SMTP Server, sistemi i llogarive duhet të inicializohet';
+$lang['smtp_app_description'] = 'SMTP Server ofron një server hyrëse dhe dalëse të postës, si dhe forwarding mail dhe autentikimit SMTP karakteristika.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain Text-Passwords';
+$lang['smtp_configure_accounts'] = 'Llogaritë Konfiguro';
+$lang['smtp_destination_domains'] = 'Fushat Destinacioni';
+$lang['smtp_failed_login_detection'] = 'SMTP Dështoi Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail mohim është i pavlefshëm.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'settings hostname dhe domain mund të konfigurohet në aplikacionin Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Maximum Size mesazh';
+$lang['smtp_port_invalid'] = 'Port është e pavlefshme.';
+$lang['smtp_recipient_mapping_invalid'] = 'Marrësi mapping është e pavlefshme.';
+$lang['smtp_relay_host_invalid'] = 'Relay host është i pavlefshëm.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server është i pavlefshëm.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server Target';
+$lang['smtp_trusted_network_invalid'] = 'Rrjeti i besuar është e pavlefshme.';
+$lang['smtp_trusted_networks'] = 'Rrjetet besuar';
+$lang['smtp_user_policies'] = 'Politikat e përdoruesit';
