124bb725a7b24059db3deb78dfb363cde08244bd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:21:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sl/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Da bi vse funkcije v SMTP Server, mora biti sistem računi inicializirati';
+$lang['smtp_app_description'] = 'SMTP Server omogoča dohodne in odhodne poštnega strežnika, kot tudi posredovanje pošte in preverjanje pristnosti SMTP funkcije.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text gesla';
+$lang['smtp_configure_accounts'] = 'Konfiguracija računi';
+$lang['smtp_destination_domains'] = 'Ciljni domene';
+$lang['smtp_failed_login_detection'] = 'SMTP ni uspelo prijave';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer je neveljavna.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Nastavitve imen gostiteljev in domen se lahko nastavijo v aplikaciji Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Največja velikost sporočilo';
+$lang['smtp_port_invalid'] = 'Port je neveljavna.';
+$lang['smtp_recipient_mapping_invalid'] = 'Prejemnik kartiranje je neveljavna.';
+$lang['smtp_relay_host_invalid'] = 'Rele gostitelj je neveljavna.';
+$lang['smtp_relay_host'] = 'rele Host';
+$lang['smtp_server_invalid'] = 'SMTP Server je neveljavna.';
+$lang['smtp_smtp_authentication'] = 'SMTP avtentikacija';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Ciljna Server';
+$lang['smtp_trusted_network_invalid'] = 'Trusted omrežje je neveljavna.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'Uporabniške Politike';
