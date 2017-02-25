a5b81671d8ef8cb105169fbe959b9a1e689b14df
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ca/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Per tal d&#39;activar totes les funcions del servidor SMTP, el sistema de comptes ha de ser inicialitzat';
+$lang['smtp_app_description'] = 'El servidor SMTP proporciona un servidor de correu entrant i sortint, així com funcions de reenviament de correu electrònic i autenticació SMTP.';
+$lang['smtp_app_name'] = 'servidor SMTP';
+$lang['smtp_block_plaintext'] = 'Bloc de text sense format contrasenyes';
+$lang['smtp_configure_accounts'] = 'Configura comptes';
+$lang['smtp_destination_domains'] = 'Dominis de destinació';
+$lang['smtp_failed_login_detection'] = 'Inicis de sessió SMTP Error';
+$lang['smtp_mail_disclaimer_invalid'] = 'descàrrec de responsabilitat electrònic no és vàlida.';
+$lang['smtp_mail_forwarding'] = 'Transmissió de correu';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Els ajustos de màquina o domini es poden configurar en l&#39;aplicació de configuració de missatgeria.';
+$lang['smtp_maximum_message_size'] = 'Mida màxima del missatge';
+$lang['smtp_port_invalid'] = 'El port no és vàlid.';
+$lang['smtp_recipient_mapping_invalid'] = 'mapeig destinatari no és vàlid.';
+$lang['smtp_relay_host_invalid'] = 'Host de retransmissió no és vàlid.';
+$lang['smtp_relay_host'] = 'Host de retransmissió';
+$lang['smtp_server_invalid'] = 'Servidor SMTP no és vàlid.';
+$lang['smtp_smtp_authentication'] = 'autenticació SMTP';
+$lang['smtp_smtp_server'] = 'servidor SMTP';
+$lang['smtp_target_server'] = 'servidor de destinació';
+$lang['smtp_trusted_network_invalid'] = 'Xarxa de confiança no és vàlid.';
+$lang['smtp_trusted_networks'] = 'Xarxes de confiança';
+$lang['smtp_user_policies'] = 'Directives d&#39;usuari';
