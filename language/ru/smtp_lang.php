ef6930b4a72ac13c6a89cd5913285c32839ade55
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:20:55 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ru/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Для того чтобы включить все функции в SMTP-сервер, система счетов должна быть инициализирована';
+$lang['smtp_app_description'] = 'Сервер SMTP обеспечивает входящий и исходящий почтовый сервер, а также пересылка почты и аутентификации SMTP функции.';
+$lang['smtp_app_name'] = 'SMTP-сервер';
+$lang['smtp_block_plaintext'] = 'Блок простые текстовые пароли';
+$lang['smtp_configure_accounts'] = 'Настройка учетных записей';
+$lang['smtp_destination_domains'] = 'Направление Домены';
+$lang['smtp_failed_login_detection'] = 'SMTP Ошибка Логины';
+$lang['smtp_mail_disclaimer_invalid'] = 'Почта отказ от ответственности является недействительным.';
+$lang['smtp_mail_forwarding'] = 'переадресация почты';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Параметры хоста и домен можно настроить в настройках приложения Mail.';
+$lang['smtp_maximum_message_size'] = 'Максимальный размер сообщения';
+$lang['smtp_port_invalid'] = 'Порт является недействительным.';
+$lang['smtp_recipient_mapping_invalid'] = 'Отображение получателей является недействительным.';
+$lang['smtp_relay_host_invalid'] = 'Реле хоста недействительно.';
+$lang['smtp_relay_host'] = 'реле хоста';
+$lang['smtp_server_invalid'] = 'Сервер SMTP является недопустимым.';
+$lang['smtp_smtp_authentication'] = 'Аутентификация SMTP';
+$lang['smtp_smtp_server'] = 'SMTP-сервер';
+$lang['smtp_target_server'] = 'Целевой сервер';
+$lang['smtp_trusted_network_invalid'] = 'Доверенные сети является недопустимым.';
+$lang['smtp_trusted_networks'] = 'Доверенные сети';
+$lang['smtp_user_policies'] = 'Правила для пользователей';
