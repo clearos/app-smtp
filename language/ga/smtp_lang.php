7cbac0c8a853703b1d6fd4c97cb30ee2702cb361
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ga/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Chun a chumasú na gnéithe sa Freastalaí SMTP, ní mór don chóras cuntais initialized';
+$lang['smtp_app_description'] = 'Cuireann an Freastalaí SMTP freastalaí ríomhphoist isteach agus amach chomh maith le gnéithe ar aghaidh ríomhphoist agus fíordheimhnithe SMTP.';
+$lang['smtp_app_name'] = 'SMTP Freastalaí';
+$lang['smtp_block_plaintext'] = 'Bloc Plain-Text Pasfhocail';
+$lang['smtp_configure_accounts'] = 'Cuntais Cumraigh';
+$lang['smtp_destination_domains'] = 'Fearainn Scríbe';
+$lang['smtp_failed_login_detection'] = 'SMTP Theip logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Is Mail séanadh neamhbhailí.';
+$lang['smtp_mail_forwarding'] = 'Mail Aghaidh';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Is féidir na socruithe óstainm agus fearainn a chumrú sa app Socruithe Ríomhphost.';
+$lang['smtp_maximum_message_size'] = 'Teachtaireacht Uasmhéid';
+$lang['smtp_port_invalid'] = 'Is Port neamhbhailí.';
+$lang['smtp_recipient_mapping_invalid'] = 'Is mapáil Faighteoir neamhbhailí.';
+$lang['smtp_relay_host_invalid'] = 'Is Leaschraolacháin óstach neamhbhailí.';
+$lang['smtp_relay_host'] = 'Relay Óstach';
+$lang['smtp_server_invalid'] = 'Is SMTP Freastalaí neamhbhailí.';
+$lang['smtp_smtp_authentication'] = 'SMTP Fíordheimhniú';
+$lang['smtp_smtp_server'] = 'SMTP Freastalaí';
+$lang['smtp_target_server'] = 'sprioc Freastalaí';
+$lang['smtp_trusted_network_invalid'] = 'Is líonra Iontaofa neamhbhailí.';
+$lang['smtp_trusted_networks'] = 'Líonraí iontaofa';
+$lang['smtp_user_policies'] = 'Polasaithe Úsáideoir';
