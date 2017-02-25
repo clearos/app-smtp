4fd4222b9d29e789419519867a3f7e6dfe572e43
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:21:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/so/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Si aad u awood oo dhan muuqaalada ee Server SMTP ah, nidaamka xisaabaadka waa in lagu initialized';
+$lang['smtp_app_description'] = 'Server SMTP wuxuu bixiyaa server mail soo socda iyo xilka ah iyo sidoo kale muuqaalada mail soo gudbiyo iyo sugida SMTP.';
+$lang['smtp_app_name'] = 'Server SMTP';
+$lang['smtp_block_plaintext'] = 'Block Plain Text-Passwords';
+$lang['smtp_configure_accounts'] = 'reserved Accounts';
+$lang['smtp_destination_domains'] = 'Domains Destination';
+$lang['smtp_failed_login_detection'] = 'SMTP fashilmay magacyo';
+$lang['smtp_mail_disclaimer_invalid'] = 'Afeef Mail waa baadil.';
+$lang['smtp_mail_forwarding'] = 'Mail gudbinta';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'goobaha hostname iyo domain waxaa lagu qaybiyay kartaa app Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Size Message ugu badan';
+$lang['smtp_port_invalid'] = 'Port waa baadil.';
+$lang['smtp_recipient_mapping_invalid'] = 'khariidaynta Qaataha waa baadil.';
+$lang['smtp_relay_host_invalid'] = 'Relay ciidankii waa baadil.';
+$lang['smtp_relay_host'] = 'Relay martigeliyaha';
+$lang['smtp_server_invalid'] = 'Server SMTP waa baadil.';
+$lang['smtp_smtp_authentication'] = 'SMTP HUBINTA';
+$lang['smtp_smtp_server'] = 'Server SMTP';
+$lang['smtp_target_server'] = 'Server Target';
+$lang['smtp_trusted_network_invalid'] = 'network Kalsoonyahay oo waa baadil.';
+$lang['smtp_trusted_networks'] = 'Networks Kalsoonyahay';
+$lang['smtp_user_policies'] = 'Siyaasadaha User';
