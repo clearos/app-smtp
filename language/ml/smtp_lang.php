5dd5978b58eeba1af932a1cc01ea37d657c6e3d2
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ml/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP സെർവർ എല്ലാ സാധ്യമാക്കുന്നതിന് വേണ്ടി, അക്കൗണ്ടുകൾ സിസ്റ്റം സമാരംഭിച്ചിട്ടില്ല വേണം';
+$lang['smtp_app_description'] = 'SMTP സെർവർ ഒരു ഇൻകമിംഗ്, ഔട്ട്ഗോയിംഗ് മെയിൽ സെർവർ അതുപോലെ മെയിൽ കൈമാറൽ, എസ്എംടിപിസെര്വര് ആധികാരികത സവിശേഷതകൾ നൽകുന്നു.';
+$lang['smtp_app_name'] = 'SMTP സെർവർ';
+$lang['smtp_block_plaintext'] = 'തടയുക പ്ലെയിൻ ടെക്സ്റ്റ് പാസ്വേഡുകൾ';
+$lang['smtp_configure_accounts'] = 'അക്കൗണ്ടുകൾ ക്രമീകരിക്കുക';
+$lang['smtp_destination_domains'] = 'ലക്ഷ്യസ്ഥാനം ഡൊമെയ്നുകൾ';
+$lang['smtp_failed_login_detection'] = 'എസ്എംപിടി പരാജയപ്പെട്ടു പ്രവേശിക്കലുകൾ';
+$lang['smtp_mail_disclaimer_invalid'] = 'മെയിൽ നിരാകരണം അസാധുവാണ്.';
+$lang['smtp_mail_forwarding'] = 'മെയിൽ കൈമാറ്റം';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ഹോസ്റ്റ്നെയിമും ഡൊമെയ്ൻ ക്രമീകരണങ്ങൾ മെയിൽ ക്രമീകരണങ്ങൾ അപ്ലിക്കേഷൻ ക്രമീകരിക്കാം.';
+$lang['smtp_maximum_message_size'] = 'പരമാവധി സന്ദേശ വലുപ്പം';
+$lang['smtp_port_invalid'] = 'പോര്ട് അസാധുവാണ്.';
+$lang['smtp_recipient_mapping_invalid'] = 'സ്വീകർത്താവിന്റെ മാപ്പിംഗ് അസാധുവാണ്.';
+$lang['smtp_relay_host_invalid'] = 'റിലേ ഹോസ്റ്റ് അസാധുവാണ്.';
+$lang['smtp_relay_host'] = 'റിലേ ഹോസ്റ്റ്';
+$lang['smtp_server_invalid'] = 'SMTP സെർവർ അസാധുവാണ്.';
+$lang['smtp_smtp_authentication'] = 'എസ്എംടിപിസെര്വര് ആധികാരികത';
+$lang['smtp_smtp_server'] = 'SMTP സെർവർ';
+$lang['smtp_target_server'] = 'ടാർഗെറ്റ് സെർവർ';
+$lang['smtp_trusted_network_invalid'] = 'വിശ്വസനീയ നെറ്റ്വർക്ക് അസാധുവാണ്.';
+$lang['smtp_trusted_networks'] = 'വിശ്വസനീയ നെറ്റ്വർക്കുകൾ';
+$lang['smtp_user_policies'] = 'ഉപയോക്തൃ നയങ്ങൾ';
