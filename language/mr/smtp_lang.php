34b533703fc646cc8bcf98ec9aa9553ba599d940
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:24 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mr/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'या SMTP सर्व्हर सर्व वैशिष्ट्ये सक्षम करण्यासाठी, खाती प्रणाली सुरू करणे आवश्यक आहे';
+$lang['smtp_app_description'] = 'या SMTP सर्व्हर एक येणारे आणि जाणारे मेल सर्व्हर तसेच मेल अग्रेषित करणे आणि या SMTP प्रमाणीकरण गुणविशेष पुरवतो.';
+$lang['smtp_app_name'] = 'या SMTP सर्व्हर';
+$lang['smtp_block_plaintext'] = 'ब्लॉक साधा मजकूर संकेतशब्द';
+$lang['smtp_configure_accounts'] = 'खाती संरचीत करा';
+$lang['smtp_destination_domains'] = 'गंतव्य डोमेन';
+$lang['smtp_failed_login_detection'] = 'या SMTP अपयशी लॉगिन';
+$lang['smtp_mail_disclaimer_invalid'] = 'मेल अस्वीकरण अवैध आहे.';
+$lang['smtp_mail_forwarding'] = 'मेलचे अग्रेषण';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'यजमाननाव डोमेन सेटिंग्ज मेल सेटिंग्ज अॅप मध्ये संरचीत करणे शक्य आहे.';
+$lang['smtp_maximum_message_size'] = 'कमाल संदेश आकार';
+$lang['smtp_port_invalid'] = 'पोर्ट अवैध आहे.';
+$lang['smtp_recipient_mapping_invalid'] = 'प्राप्तकर्ता मॅपिंग अवैध आहे.';
+$lang['smtp_relay_host_invalid'] = 'रिले यजमान अवैध आहे.';
+$lang['smtp_relay_host'] = 'रिले होस्ट';
+$lang['smtp_server_invalid'] = 'या SMTP सर्व्हर अवैध आहे.';
+$lang['smtp_smtp_authentication'] = 'या SMTP प्रमाणीकरण';
+$lang['smtp_smtp_server'] = 'या SMTP सर्व्हर';
+$lang['smtp_target_server'] = 'लक्ष्य सर्व्हर';
+$lang['smtp_trusted_network_invalid'] = 'विश्वसनीय नेटवर्क अवैध आहे.';
+$lang['smtp_trusted_networks'] = 'विश्वसनीय नेटवर्क';
+$lang['smtp_user_policies'] = 'वापरकर्ता धोरणे';
