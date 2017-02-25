fd367ccf5c257f311942000e381cee03d347ff70
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:20:22 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fa/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'به منظور فعال کردن تمام ویژگی های در SMTP سرور، سیستم حساب باید آغازینه شود';
+$lang['smtp_app_description'] = 'سرور SMTP سرور ایمیل های ورودی و خروجی و همچنین ویژگی های حمل و نقل ایمیل و احراز هویت SMTP فراهم می کند.';
+$lang['smtp_app_name'] = 'SMTP سرور';
+$lang['smtp_block_plaintext'] = 'بلوک ساده متن کلمه عبور';
+$lang['smtp_configure_accounts'] = 'حساب پیکربندی';
+$lang['smtp_destination_domains'] = 'دامنه مقصد';
+$lang['smtp_failed_login_detection'] = 'SMTP نشد باری';
+$lang['smtp_mail_disclaimer_invalid'] = 'ایمیل سلب مسئولیت نامعتبر است.';
+$lang['smtp_mail_forwarding'] = 'فوروارد ایمیل';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'تنظیمات نام میزبان و دامنه را می توان در برنامه تنظیمات ایمیل پیکربندی شده است.';
+$lang['smtp_maximum_message_size'] = 'پیام حداکثر اندازه';
+$lang['smtp_port_invalid'] = 'بندر نامعتبر است.';
+$lang['smtp_recipient_mapping_invalid'] = 'نقشه برداری گیرنده نامعتبر است.';
+$lang['smtp_relay_host_invalid'] = 'میزبان رله نامعتبر است.';
+$lang['smtp_relay_host'] = 'میزبان رله';
+$lang['smtp_server_invalid'] = 'SMTP سرور نامعتبر است.';
+$lang['smtp_smtp_authentication'] = 'احراز هویت SMTP';
+$lang['smtp_smtp_server'] = 'SMTP سرور';
+$lang['smtp_target_server'] = 'سرور هدف';
+$lang['smtp_trusted_network_invalid'] = 'شبکه مورد اعتماد نامعتبر است.';
+$lang['smtp_trusted_networks'] = 'شبکه های مورد اعتماد';
+$lang['smtp_user_policies'] = 'خط مشیهای کاربر';
