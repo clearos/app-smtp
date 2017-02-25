fc3d5b218135b76ac7a89de236de76bba4c8bcaa
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:21:11 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/si/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'මෙම SMTP සේවාදායකය සියලු අංග සක්රීය කිරීම සඳහා, ගිණුම් පද්ධතිය ආරම්භනය කල යුතු';
+$lang['smtp_app_description'] = 'මෙම SMTP සේවාදායකය ක රටින් පිටට හා රට තුළට තැපැල් සේවාදායකය මෙන්ම, තැපැල් භාණ්ඩ හරවා සහ SMTP සත්යාපනය ලක්ෂණ සපයයි.';
+$lang['smtp_app_name'] = 'SMTP සේවාදායකය';
+$lang['smtp_block_plaintext'] = 'වාරණ තැනිතලාව-පෙළ මුර පදය';
+$lang['smtp_configure_accounts'] = 'ගිණුම් මානන්න';
+$lang['smtp_destination_domains'] = 'ගමනාන්තය වසම්';
+$lang['smtp_failed_login_detection'] = 'SMTP අසාර්ථක උත්සහයන්';
+$lang['smtp_mail_disclaimer_invalid'] = 'තැපැල් ෙහෝ තනිව භාවිත වලංගු නැත.';
+$lang['smtp_mail_forwarding'] = 'තැපැල් යොමුකිරීම';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ධාරක නාමය හා වසමේ සැකසුම් ඇති තැපැල් සැකසුම් යෙදුම තුළ මානකරණය කළ හැක.';
+$lang['smtp_maximum_message_size'] = 'උපරිම පණිවුඩය ප්රමාණය';
+$lang['smtp_port_invalid'] = 'වරාය වලංගු නැත.';
+$lang['smtp_recipient_mapping_invalid'] = 'පලමු වරට පිරිනැමු සිතියම්ගත වලංගු නැත.';
+$lang['smtp_relay_host_invalid'] = 'සහය දිවීමේ සත්කාරක වලංගු නැත.';
+$lang['smtp_relay_host'] = 'සහය දිවීමේ සත්කාරක';
+$lang['smtp_server_invalid'] = 'SMTP සේවාදායකය වලංගු නැත.';
+$lang['smtp_smtp_authentication'] = 'SMTP සත්යාපනය';
+$lang['smtp_smtp_server'] = 'SMTP සේවාදායකය';
+$lang['smtp_target_server'] = 'ඉලක්ක සේවාදායකය';
+$lang['smtp_trusted_network_invalid'] = 'විශ්වාසී ජාලය වලංගු නැත.';
+$lang['smtp_trusted_networks'] = 'විශ්වාසී නෙට්වර්ක්ස්';
+$lang['smtp_user_policies'] = 'පරිශීලක ප්රතිපත්ති';
