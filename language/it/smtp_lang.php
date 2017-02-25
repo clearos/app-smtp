9b508afc58a1dd854d435e44ffdf2e471b6937a6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:15 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/it/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Al fine di consentire tutte le funzioni del server SMTP, il sistema contabile deve essere inizializzato';
+$lang['smtp_app_description'] = 'Il server SMTP fornisce un server di posta in entrata e in uscita così come le caratteristiche di inoltro della posta e autenticazione SMTP.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Blocco testo semplice password';
+$lang['smtp_configure_accounts'] = 'configurare gli account';
+$lang['smtp_destination_domains'] = 'domini di destinazione';
+$lang['smtp_failed_login_detection'] = 'Login SMTP non riuscita';
+$lang['smtp_mail_disclaimer_invalid'] = 'Posta di responsabilità non è valida.';
+$lang['smtp_mail_forwarding'] = 'mail Forward';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Le impostazioni del nome host e di dominio possono essere configurati in app Impostazioni di Posta.';
+$lang['smtp_maximum_message_size'] = 'Dimensione massima dei messaggi';
+$lang['smtp_port_invalid'] = 'Port non è valido.';
+$lang['smtp_recipient_mapping_invalid'] = 'mappatura destinatario non è valido.';
+$lang['smtp_relay_host_invalid'] = 'relay host non è valido.';
+$lang['smtp_relay_host'] = 'relay host';
+$lang['smtp_server_invalid'] = 'Server SMTP non è valido.';
+$lang['smtp_smtp_authentication'] = 'autenticazione SMTP';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'target Server';
+$lang['smtp_trusted_network_invalid'] = 'Rete attendibile non è valido.';
+$lang['smtp_trusted_networks'] = 'Reti attendibili';
+$lang['smtp_user_policies'] = 'Politiche utente';
