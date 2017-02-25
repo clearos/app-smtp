325e31f7dff23c2b8c32683036b598427e4267ee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/uz/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP Server barcha xususiyatlarini faollashtirish uchun, hisob tizimi boshlash kerak';
+$lang['smtp_app_description'] = 'SMTP Server Kiruvchi va chiquvchi pochta serverini shuningdek mail ekspeditorlik va SMTP autentifikatsiya xususiyatlari beradi.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Blok Plain matnli Parollar';
+$lang['smtp_configure_accounts'] = 'Configure Hisob';
+$lang['smtp_destination_domains'] = 'Destination Maydonlar';
+$lang['smtp_failed_login_detection'] = 'SMTP bo&#39;lmadi Kirish soni';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail Voz kechish haqiqiy emas.';
+$lang['smtp_mail_forwarding'] = 'mail yo&#39;naltirish';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'hostname va domen sozlamalari Mail Sozlamalar ilovasida tuzilgan mumkin.';
+$lang['smtp_maximum_message_size'] = 'Maksimal xabar hajmi';
+$lang['smtp_port_invalid'] = 'Port haqiqiy emas.';
+$lang['smtp_recipient_mapping_invalid'] = 'Oluvchi xaritalash haqiqiy emas.';
+$lang['smtp_relay_host_invalid'] = 'Relay mezbon haqiqiy emas.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server haqiqiy emas.';
+$lang['smtp_smtp_authentication'] = 'SMTP haqiqiylikni tekshirish';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Ishonchli tarmoq haqiqiy emas.';
+$lang['smtp_trusted_networks'] = 'ishonchli Networks';
+$lang['smtp_user_policies'] = 'foydalanuvchi siyosat';
