666da4cac1fecef6179a6bf7b8f33ee3675d109e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:24:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yo/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Ni ibere lati jeki gbogbo awọn ẹya ara ni awọn SMTP Server, awọn iroyin eto gbọdọ wa ni initialized';
+$lang['smtp_app_description'] = 'Awọn SMTP Server pese ohun nwọle ti njade ati mail server bi daradara bi mail firanšẹ siwaju ati SMTP ìfàṣẹsí ẹya ara ẹrọ.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text ọrọigbaniwọle';
+$lang['smtp_configure_accounts'] = 'atunto iroyin';
+$lang['smtp_destination_domains'] = 'nlo ibugbe';
+$lang['smtp_failed_login_detection'] = 'SMTP kùnà logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail be ni invalid.';
+$lang['smtp_mail_forwarding'] = 'mail Ndari';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Awọn hostname ati ašẹ eto le wa ni tunto ninu awọn Mail Eto app.';
+$lang['smtp_maximum_message_size'] = 'O pọju Ifiranṣẹ Iwon';
+$lang['smtp_port_invalid'] = 'Port ni invalid.';
+$lang['smtp_recipient_mapping_invalid'] = 'Olugba aworan ni invalid.';
+$lang['smtp_relay_host_invalid'] = 'Yii ogun ni invalid.';
+$lang['smtp_relay_host'] = 'yii Gbalejo';
+$lang['smtp_server_invalid'] = 'SMTP Server ni invalid.';
+$lang['smtp_smtp_authentication'] = 'SMTP Ijeri';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Àkọlé Server';
+$lang['smtp_trusted_network_invalid'] = 'Gbẹkẹle nẹtiwọki ni invalid.';
+$lang['smtp_trusted_networks'] = 'gbẹkẹle nẹtiwọki';
+$lang['smtp_user_policies'] = 'User imulo';
