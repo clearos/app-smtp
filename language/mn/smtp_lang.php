c3fc34058d82b57b6c06a4febec984fed02b74e8
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mn/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP сервер бүхий л боломжуудыг идэвхжүүлж тулд данс систем эхэлж байх ёстой';
+$lang['smtp_app_description'] = 'SMTP сервер нь орж ирж байгаа болон гарч байгаа захидлын сервер, түүнчлэн мэйл зуучлал, SMTP нэвтрэлт танилт боломжуудыг олгодог.';
+$lang['smtp_app_name'] = 'SMTP сервер';
+$lang['smtp_block_plaintext'] = 'Блок Цэвэр текст нууц үг';
+$lang['smtp_configure_accounts'] = 'тохируулах данс';
+$lang['smtp_destination_domains'] = 'Destination домэйн';
+$lang['smtp_failed_login_detection'] = 'SMTP Логин амжилтгүй болсон';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail түгээнэ хүчин төгөлдөр бус байна.';
+$lang['smtp_mail_forwarding'] = 'Захидал зуучийн';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'хостын нэр болон домэйн тохиргоо нь Захидал Тохиргоо програм нь тохируулагдаж болно.';
+$lang['smtp_maximum_message_size'] = 'Хамгийн их Зурвас Хэмжээ';
+$lang['smtp_port_invalid'] = 'Порт хүчин төгөлдөр бус байна.';
+$lang['smtp_recipient_mapping_invalid'] = 'Хүлээн авагч зураглал хүчин төгөлдөр бус байна.';
+$lang['smtp_relay_host_invalid'] = 'Relay хост хүчин төгөлдөр бус байна.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP сервер хүчин төгөлдөр бус байна.';
+$lang['smtp_smtp_authentication'] = 'SMTP нэвтрэлт танилт';
+$lang['smtp_smtp_server'] = 'SMTP сервер';
+$lang['smtp_target_server'] = 'зорилтот Server';
+$lang['smtp_trusted_network_invalid'] = 'Итгэмжлэгдсэн сүлжээний хүчин төгөлдөр бус байна.';
+$lang['smtp_trusted_networks'] = 'итгэмжлэгдсэн Networks';
+$lang['smtp_user_policies'] = 'Хэрэглэгчийн бодлого';
