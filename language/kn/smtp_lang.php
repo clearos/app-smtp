840c15c513180b8e82ba227fe77e8bdaadd3051a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:56 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kn/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP ಸರ್ವರ್ ಎಲ್ಲಾ ವೈಶಿಷ್ಟ್ಯಗಳನ್ನು ಶಕ್ತಗೊಳಿಸಲು ಸಲುವಾಗಿ, ಖಾತೆಗಳನ್ನು ವ್ಯವಸ್ಥೆಯ ಆರಂಭಿಸ ಬೇಕು';
+$lang['smtp_app_description'] = 'SMTP ಸರ್ವರ್ ಒಳಬರುವ ಮತ್ತು ಹೊರಹೋಗುವ ಮೇಲ್ ಸರ್ವರ್ ಹಾಗೂ ಮೇಲ್ ಫಾರ್ವರ್ಡ್ ಮತ್ತು SMTP ದೃಢೀಕರಣ ಲಕ್ಷಣಗಳನ್ನು ಒದಗಿಸುತ್ತದೆ.';
+$lang['smtp_app_name'] = 'SMTP ಸರ್ವರ್';
+$lang['smtp_block_plaintext'] = 'ಬ್ಲಾಕ್ ಸರಳ ಪಠ್ಯ ಪಾಸ್ವರ್ಡ್ಗಳು';
+$lang['smtp_configure_accounts'] = 'ಕಾನ್ಫಿಗರ್ ಖಾತೆಗಳು';
+$lang['smtp_destination_domains'] = 'ಗಮ್ಯಸ್ಥಾನ ಡೊಮೇನ್ಗಳ';
+$lang['smtp_failed_login_detection'] = 'ನಿಮ್ಮ SMTP ವಿಫಲವಾಗಿದೆ ಲಾಗಿನ್ನುಗಳು';
+$lang['smtp_mail_disclaimer_invalid'] = 'ಮೇಲ್ ಹಕ್ಕುತ್ಯಾಗ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['smtp_mail_forwarding'] = 'ಮೇಲ್ ಫಾರ್ವರ್ಡಿಂಗ್';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ಹೋಸ್ಟ್ ಮತ್ತು ಡೊಮೇನ್ ಸೆಟ್ಟಿಂಗ್ಗಳನ್ನು ಮೇಲ್ ಸೆಟ್ಟಿಂಗ್ಗಳು ಅಪ್ಲಿಕೇಶನ್ ಸಂರಚಿಸಬಹುದು.';
+$lang['smtp_maximum_message_size'] = 'ಗರಿಷ್ಠ ಸಂದೇಶ ಗಾತ್ರ';
+$lang['smtp_port_invalid'] = 'ಪೋರ್ಟ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['smtp_recipient_mapping_invalid'] = 'ಸ್ವೀಕರಿಸುವವರ ಮ್ಯಾಪಿಂಗ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['smtp_relay_host_invalid'] = 'ರಿಲೇ ಹೋಸ್ಟ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['smtp_relay_host'] = 'ರಿಲೇ ಹೋಸ್ಟ್';
+$lang['smtp_server_invalid'] = 'SMTP ಸರ್ವರ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['smtp_smtp_authentication'] = 'SMTP ದೃಢೀಕರಣ';
+$lang['smtp_smtp_server'] = 'SMTP ಸರ್ವರ್';
+$lang['smtp_target_server'] = 'ಟಾರ್ಗೆಟ್ ಸರ್ವರ್';
+$lang['smtp_trusted_network_invalid'] = 'ಟ್ರಸ್ಟೆಡ್ ನೆಟ್ವರ್ಕ್ ಅಮಾನ್ಯವಾಗಿದೆ.';
+$lang['smtp_trusted_networks'] = 'ಟ್ರಸ್ಟೆಡ್ ನೆಟ್ವರ್ಕ್ಸ್';
+$lang['smtp_user_policies'] = 'ಬಳಕೆದಾರನ ನೀತಿಗಳು';
