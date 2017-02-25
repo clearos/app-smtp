4c1bc46fd329e8a411fabaf443f3d1da734d7adb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ka/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'იმისათვის, რომ ყველა იმ თვისებით, SMTP სერვერი, ანგარიშები, სისტემა უნდა ინიციალიზაცია';
+$lang['smtp_app_description'] = 'SMTP სერვერი უზრუნველყოფს შემომავალი და გამავალი ფოსტის სერვერზე, ასევე გადაგზავნის და SMTP ავტორიზაციის თვისებები.';
+$lang['smtp_app_name'] = 'SMTP სერვერი';
+$lang['smtp_block_plaintext'] = 'Block ტექსტური პაროლები';
+$lang['smtp_configure_accounts'] = 'კონფიგურაცია ანგარიშები';
+$lang['smtp_destination_domains'] = 'დანიშნულების Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP ვერ Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'ფოსტა პასუხისმგებლობის არასწორია.';
+$lang['smtp_mail_forwarding'] = 'mail გადამისამართება';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ჰოსტის და დომენის პარამეტრები შეიძლება კონფიგურაცია ფოსტის პარამეტრები app.';
+$lang['smtp_maximum_message_size'] = 'მაქსიმალური გაგზავნა ზომა';
+$lang['smtp_port_invalid'] = 'Port არასწორია.';
+$lang['smtp_recipient_mapping_invalid'] = 'მიმღების რუკების არასწორია.';
+$lang['smtp_relay_host_invalid'] = 'Relay მასპინძელი არასწორია.';
+$lang['smtp_relay_host'] = 'relay მასპინძელი';
+$lang['smtp_server_invalid'] = 'SMTP სერვერი არასწორია.';
+$lang['smtp_smtp_authentication'] = 'SMTP ავთენტიფიკაცია';
+$lang['smtp_smtp_server'] = 'SMTP სერვერი';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'სანდო ქსელის არასწორია.';
+$lang['smtp_trusted_networks'] = 'სანდო ქსელები';
+$lang['smtp_user_policies'] = 'მომხმარებელი პოლიტიკის';
