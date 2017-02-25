0aa02731fa18aa81a6dced5d13b62c61aed7f397
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mi/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'I roto i te tikanga ki te taea nga āhuatanga katoa i roto i te Tūmau SMTP, me arawhiti te pūnaha pūkete';
+$lang['smtp_app_description'] = 'whakarato te Tūmau SMTP he tūmau mēra taumai me te matenga me te pai ano āhuatanga whakamua mēra, me te SMTP motuhēhēnga.';
+$lang['smtp_app_name'] = 'Tūmau SMTP';
+$lang['smtp_block_plaintext'] = 'Kupuhipa Poraka Tōkau-Kuputuhi';
+$lang['smtp_configure_accounts'] = 'Pūkete Whirihora';
+$lang['smtp_destination_domains'] = 'Ngā rohe ūnga';
+$lang['smtp_failed_login_detection'] = 'SMTP rahua takiuru';
+$lang['smtp_mail_disclaimer_invalid'] = 'He muhu whakakape mēra.';
+$lang['smtp_mail_forwarding'] = 'Mail whakamua';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Ka taea te whirihora i te tautuhinga ingoahautū me rohe i roto i te taupānga Tautuhinga Mail.';
+$lang['smtp_maximum_message_size'] = 'Rahi Karere Mōrahi';
+$lang['smtp_port_invalid'] = 'He muhu Port.';
+$lang['smtp_recipient_mapping_invalid'] = 'He muhu kaiwhiwhi whakamahere.';
+$lang['smtp_relay_host_invalid'] = 'He muhu Relay ope.';
+$lang['smtp_relay_host'] = 'Relay Kaihautū';
+$lang['smtp_server_invalid'] = 'He muhu Tūmau SMTP.';
+$lang['smtp_smtp_authentication'] = 'SMTP Motuhēhēnga';
+$lang['smtp_smtp_server'] = 'Tūmau SMTP';
+$lang['smtp_target_server'] = 'Tūmau Target';
+$lang['smtp_trusted_network_invalid'] = 'He muhu whatunga Whirinaki.';
+$lang['smtp_trusted_networks'] = 'Networks Whirinaki';
+$lang['smtp_user_policies'] = 'kaupapa here kaiwhakamahi';
