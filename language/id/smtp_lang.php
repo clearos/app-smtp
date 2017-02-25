ab0830cb08796bd94bf7a56385a468b1fb516a32
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/id/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Untuk memungkinkan semua fitur di SMTP Server, sistem rekening harus diinisialisasi';
+$lang['smtp_app_description'] = 'SMTP Server menyediakan server surat masuk dan keluar serta mail forwarding dan otentikasi SMTP fitur.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Blok Plain-Text Password';
+$lang['smtp_configure_accounts'] = 'Akun Configure';
+$lang['smtp_destination_domains'] = 'Domain tujuan';
+$lang['smtp_failed_login_detection'] = 'SMTP Gagal Login';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer tidak valid.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Hostname dan domain pengaturan dapat dikonfigurasi dalam aplikasi Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Maksimum Ukuran Pesan';
+$lang['smtp_port_invalid'] = 'Pelabuhan tidak valid.';
+$lang['smtp_recipient_mapping_invalid'] = 'pemetaan penerima tidak valid.';
+$lang['smtp_relay_host_invalid'] = 'Relay tuan rumah tidak valid.';
+$lang['smtp_relay_host'] = 'Relay host';
+$lang['smtp_server_invalid'] = 'SMTP Server tidak valid.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'jaringan terpercaya tidak valid.';
+$lang['smtp_trusted_networks'] = 'Jaringan dipercaya';
+$lang['smtp_user_policies'] = 'Kebijakan pengguna';
