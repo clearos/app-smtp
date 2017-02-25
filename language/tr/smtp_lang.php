3cf50bc68e562728ddc34708ab9d36c7b0fdfde1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tr/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP Sunucusu tüm özellikleri etkinleştirmek için, hesaplar sistemi başlatılması gerekir';
+$lang['smtp_app_description'] = 'SMTP Sunucusu gelen ve giden posta sunucusu yanı sıra e-posta yönlendirme ve SMTP kimlik özellikleri sağlar.';
+$lang['smtp_app_name'] = 'SMTP Sunucusu';
+$lang['smtp_block_plaintext'] = 'Blok Düz-Metin Şifreleri';
+$lang['smtp_configure_accounts'] = 'Yapılandırma Hesaplar';
+$lang['smtp_destination_domains'] = 'hedef Alanlar';
+$lang['smtp_failed_login_detection'] = 'SMTP Başarısız Girişler';
+$lang['smtp_mail_disclaimer_invalid'] = 'Posta reddi geçersiz.';
+$lang['smtp_mail_forwarding'] = 'mail Yönlendirme';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'hostname ve etki alanı ayarları Posta Ayarlar uygulamasında yapılandırılabilir.';
+$lang['smtp_maximum_message_size'] = 'Maksimum ileti boyutu';
+$lang['smtp_port_invalid'] = 'Liman geçersiz.';
+$lang['smtp_recipient_mapping_invalid'] = 'Alıcı haritalama geçersiz.';
+$lang['smtp_relay_host_invalid'] = 'Röle konak geçersiz.';
+$lang['smtp_relay_host'] = 'röle Sunucu';
+$lang['smtp_server_invalid'] = 'SMTP Sunucusu geçersiz.';
+$lang['smtp_smtp_authentication'] = 'SMTP Kimlik Doğrulaması';
+$lang['smtp_smtp_server'] = 'SMTP Sunucusu';
+$lang['smtp_target_server'] = 'hedef Sunucu';
+$lang['smtp_trusted_network_invalid'] = 'Güvenilir ağ geçersiz.';
+$lang['smtp_trusted_networks'] = 'güvenilir Ağlar';
+$lang['smtp_user_policies'] = 'kullanıcı Politikaları';
