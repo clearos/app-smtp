63607b615deafc03ffeb397d6345fa9d2afcfba9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ne/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP सर्भर मा सबै सुविधाहरू सक्षम गर्न, खाता प्रणाली आरम्भ हुनुपर्छ';
+$lang['smtp_app_description'] = 'SMTP सर्भर आगमन र बहिर्गमन मेल साथै मेल फर्वार्ड र SMTP प्रमाणीकरण सुविधाहरू सर्भर प्रदान गर्दछ।';
+$lang['smtp_app_name'] = 'SMTP सर्भर';
+$lang['smtp_block_plaintext'] = 'ब्लक सादा-पाठ पासवर्ड';
+$lang['smtp_configure_accounts'] = 'लेखा कन्फिगर';
+$lang['smtp_destination_domains'] = 'गन्तव्य डोमेन';
+$lang['smtp_failed_login_detection'] = 'SMTP असफल लगइनहरू';
+$lang['smtp_mail_disclaimer_invalid'] = 'मेल त्याग अमान्य छ।';
+$lang['smtp_mail_forwarding'] = 'मेल फरवार्ड';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'होस्टनाम र डोमेन सेटिङ मेल सेटिङहरू अनुप्रयोगमा कन्फिगर गर्न सकिन्छ।';
+$lang['smtp_maximum_message_size'] = 'अधिकतम सन्देश आकार';
+$lang['smtp_port_invalid'] = 'पोर्ट अमान्य छ।';
+$lang['smtp_recipient_mapping_invalid'] = 'प्राप्तकर्ता म्यापिङ अमान्य छ।';
+$lang['smtp_relay_host_invalid'] = 'रिले पाहुना अमान्य छ।';
+$lang['smtp_relay_host'] = 'रिले होस्ट';
+$lang['smtp_server_invalid'] = 'SMTP सर्भर अमान्य छ।';
+$lang['smtp_smtp_authentication'] = 'SMTP प्रमाणीकरण';
+$lang['smtp_smtp_server'] = 'SMTP सर्भर';
+$lang['smtp_target_server'] = 'लक्ष्य सर्भर';
+$lang['smtp_trusted_network_invalid'] = 'विश्वसनीय नेटवर्क अमान्य छ।';
+$lang['smtp_trusted_networks'] = 'विश्वसनीय नेटवर्क';
+$lang['smtp_user_policies'] = 'प्रयोगकर्ता नीतिहरू';
