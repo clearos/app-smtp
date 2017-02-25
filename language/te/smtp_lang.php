3f912ac083ea6b38b0a4dff70a34e954fb560201
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:34 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/te/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP సర్వర్ లో అన్ని ఫీచర్లను ఎనేబుల్ చేయడానికి, ఖాతాల వ్యవస్థ initialized తప్పక';
+$lang['smtp_app_description'] = 'SMTP సర్వర్ ఇన్కమింగ్ మరియు అవుట్గోయింగ్ మెయిల్ సర్వర్ అలాగే మెయిల్ ఫార్వర్డ్ మరియు SMTP ప్రమాణీకరణ ఫీచర్లను అందిస్తుంది.';
+$lang['smtp_app_name'] = 'SMTP సర్వర్';
+$lang['smtp_block_plaintext'] = 'బ్లాక్ సాదా టెక్స్ట్ పాస్వర్డ్లు';
+$lang['smtp_configure_accounts'] = 'అకౌంట్స్ అమర్చు &#39;';
+$lang['smtp_destination_domains'] = 'గమ్యం డొమైన్';
+$lang['smtp_failed_login_detection'] = 'SMTP విఫలమైంది లాగిన్';
+$lang['smtp_mail_disclaimer_invalid'] = 'మెయిల్ డిస్క్లైమర్ చెల్లదు.';
+$lang['smtp_mail_forwarding'] = 'మెయిల్ ఫార్వార్డింగ్';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'హోస్టునామము మరియు డొమైన్ సెట్టింగులు మెయిల్ సెట్టింగ్ల అనువర్తనాన్ని లో అమర్చవచ్చు.';
+$lang['smtp_maximum_message_size'] = 'గరిష్ట సందేశ పరిమాణం';
+$lang['smtp_port_invalid'] = 'పోర్ట్ చెల్లదు.';
+$lang['smtp_recipient_mapping_invalid'] = 'గ్రహీత మ్యాపింగ్ చెల్లదు.';
+$lang['smtp_relay_host_invalid'] = 'రిలే హోస్ట్ చెల్లదు.';
+$lang['smtp_relay_host'] = 'రిలే హోస్ట్';
+$lang['smtp_server_invalid'] = 'SMTP సర్వర్ చెల్లదు.';
+$lang['smtp_smtp_authentication'] = 'SMTP ప్రమాణీకరణ';
+$lang['smtp_smtp_server'] = 'SMTP సర్వర్';
+$lang['smtp_target_server'] = 'టార్గెట్ సర్వర్';
+$lang['smtp_trusted_network_invalid'] = 'విశ్వసనీయ నెట్వర్క్ చెల్లదు.';
+$lang['smtp_trusted_networks'] = 'విశ్వసనీయ నెట్వర్క్స్';
+$lang['smtp_user_policies'] = 'వినియోగదారు విధానాలు';
