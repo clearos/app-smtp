1eaaba8c77a474bffe922985aad42ea1cccbe4b0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:20:14 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pa/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'ਕ੍ਰਮ SMTP ਸਰਵਰ ਦੇ ਸਾਰੇ ਫੀਚਰ ਨੂੰ ਯੋਗ ਕਰਨ ਲਈ, ਖਾਤੇ ਸਿਸਟਮ ਸ਼ੁਰੂ ਕੀਤਾ ਜਾਣਾ ਚਾਹੀਦਾ ਹੈ';
+$lang['smtp_app_description'] = 'SMTP ਸਰਵਰ ਨੂੰ ਇੱਕ ਆਉਣ ਅਤੇ ਬਾਹਰ ਜਾਣ ਮੇਲ ਸਰਵਰ ਦੇ ਨਾਲ ਨਾਲ ਮੇਲ ਫਾਰਵਰਡਿੰਗ ਨੂੰ ਅਤੇ SMTP ਪਰਮਾਣਕਿਤਾ ਫੀਚਰ ਦਿੰਦਾ ਹੈ.';
+$lang['smtp_app_name'] = 'SMTP ਸਰਵਰ';
+$lang['smtp_block_plaintext'] = 'ਬਲਾਕ ਸਾਦੇ-ਪਾਠ ਪਾਸਵਰਡ';
+$lang['smtp_configure_accounts'] = 'ਸੰਰਚਨਾ ਖਾਤੇ';
+$lang['smtp_destination_domains'] = 'ਨਿਸ਼ਾਨਾ ਡੋਮੇਨ';
+$lang['smtp_failed_login_detection'] = 'SMTP ਵਿੱਚ ਅਸਫਲ ਲਾਗਇਨ';
+$lang['smtp_mail_disclaimer_invalid'] = 'ਮੇਲ ਬੇਦਾਅਵਾ ਗਲਤ ਹੈ.';
+$lang['smtp_mail_forwarding'] = 'ਮੇਲ ਫਾਰਵਰਡਿੰਗ';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ਮੇਜ਼ਬਾਨ ਅਤੇ ਡੋਮੇਨ ਸੈਟਿੰਗ ਮੇਲ ਸੈਟਿੰਗ ਐਪ ਵਿੱਚ ਸੰਰਚਿਤ ਕੀਤਾ ਜਾ ਸਕਦਾ ਹੈ.';
+$lang['smtp_maximum_message_size'] = 'ਵੱਧ ਸੁਨੇਹਾ ਆਕਾਰ';
+$lang['smtp_port_invalid'] = 'ਪੋਰ੍ਟ ਗਲਤ ਹੈ.';
+$lang['smtp_recipient_mapping_invalid'] = 'ਪ੍ਰਾਪਤਕਰਤਾ ਮੈਪਿੰਗ ਗਲਤ ਹੈ.';
+$lang['smtp_relay_host_invalid'] = 'ਰੀਲੇਅ ਹੋਸਟ ਗਲਤ ਹੈ.';
+$lang['smtp_relay_host'] = 'ਰੀਲੇਅ ਮੇਜ਼ਬਾਨ';
+$lang['smtp_server_invalid'] = 'SMTP ਸਰਵਰ ਗਲਤ ਹੈ.';
+$lang['smtp_smtp_authentication'] = 'SMTP ਪਰਮਾਣਕਿਤਾ';
+$lang['smtp_smtp_server'] = 'SMTP ਸਰਵਰ';
+$lang['smtp_target_server'] = 'ਟੀਚੇ ਦਾ ਸਰਵਰ';
+$lang['smtp_trusted_network_invalid'] = 'ਭਰੋਸੇਯੋਗ ਨੈੱਟਵਰਕ ਗਲਤ ਹੈ.';
+$lang['smtp_trusted_networks'] = 'ਭਰੋਸੇਯੋਗ ਨੈੱਟਵਰਕ';
+$lang['smtp_user_policies'] = 'ਯੂਜ਼ਰ ਡਰਾਇਰ';
