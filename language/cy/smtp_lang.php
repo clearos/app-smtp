0c2a6a0edec9c9332363e3a6ad1634e489eb7c24
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:50 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cy/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Er mwyn galluogi&#39;r holl nodweddion yn y Gweinyddwr SMTP, rhaid i&#39;r system cyfrifon ei hymgychwyn';
+$lang['smtp_app_description'] = 'Mae&#39;r Gweinyddwr SMTP yn darparu gweinydd post sy&#39;n dod i mewn ac yn mynd allan yn ogystal â nodweddion anfon post ymlaen a dilysu SMTP.';
+$lang['smtp_app_name'] = 'Gweinydd SMTP';
+$lang['smtp_block_plaintext'] = 'Bloc Plaen-Text Passwords';
+$lang['smtp_configure_accounts'] = 'Cyfrifon Ffurfweddu';
+$lang['smtp_destination_domains'] = 'Parthau cyrchfan';
+$lang['smtp_failed_login_detection'] = 'Mewngofnodi SMTP Methwyd';
+$lang['smtp_mail_disclaimer_invalid'] = 'ymwadiad bost yn annilys.';
+$lang['smtp_mail_forwarding'] = 'Mail Anfon';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Gall y lleoliadau enw gwesteiwr a parth gael ei ffurfweddu yn y app Gosodiadau Mail.';
+$lang['smtp_maximum_message_size'] = 'Uchafswm Maint Neges';
+$lang['smtp_port_invalid'] = 'Port yn annilys.';
+$lang['smtp_recipient_mapping_invalid'] = 'mapio Derbynnydd yn annilys.';
+$lang['smtp_relay_host_invalid'] = 'Relay gwesteiwr yn annilys.';
+$lang['smtp_relay_host'] = 'Relay Gwesteiwr';
+$lang['smtp_server_invalid'] = 'Gweinydd SMTP yn annilys.';
+$lang['smtp_smtp_authentication'] = 'SMTP Dilysu';
+$lang['smtp_smtp_server'] = 'Gweinydd SMTP';
+$lang['smtp_target_server'] = 'Gweinydd targed';
+$lang['smtp_trusted_network_invalid'] = 'rhwydwaith Coeliedig yn annilys.';
+$lang['smtp_trusted_networks'] = 'Rhwydweithiau Trusted';
+$lang['smtp_user_policies'] = 'Polisïau defnyddiwr';
