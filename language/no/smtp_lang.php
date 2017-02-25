35fa6760c40b4e2f922e16bfc79299875eb8debd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:20:06 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/no/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'For å aktivere alle funksjonene i SMTP-server, må regnskapssystemet bli initialisert';
+$lang['smtp_app_description'] = 'SMTP-server gir et innkommende og utgående e-postserver samt videresending av post og SMTP autentisering funksjoner.';
+$lang['smtp_app_name'] = 'SMTP-server';
+$lang['smtp_block_plaintext'] = 'Block klartekst passord';
+$lang['smtp_configure_accounts'] = 'Konfigurer kontoer';
+$lang['smtp_destination_domains'] = 'destinasjon domener';
+$lang['smtp_failed_login_detection'] = 'SMTP Kunne pålogginger';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail ansvarsfraskrivelse er ugyldig.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Vertsnavn og domeneinnstillinger kan konfigureres i Mail Innstillinger-appen.';
+$lang['smtp_maximum_message_size'] = 'Maksimal meldingsstørrelse';
+$lang['smtp_port_invalid'] = 'Port er ugyldig.';
+$lang['smtp_recipient_mapping_invalid'] = 'Mottakers kartlegging er ugyldig.';
+$lang['smtp_relay_host_invalid'] = 'Relay verten er ugyldig.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP-server er ugyldig.';
+$lang['smtp_smtp_authentication'] = 'SMTP-autentisering';
+$lang['smtp_smtp_server'] = 'SMTP-server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Klarert nettverk er ugyldig.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'Brukerregler';
