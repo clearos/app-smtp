7f90cace4afc17d456cfe5161a24653e35c4d20e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ig/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Iji nwee niile atụmatụ na SMTP Server, ihe ndekọ usoro a ga-initialized';
+$lang['smtp_app_description'] = 'The SMTP Server na-enye ihe-abata na oku ozi ihe nkesa nakwa dị ka mail ebugharị na SMTP Nyocha atụmatụ.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Ngọngọ Plain-Text Okwuntughe';
+$lang['smtp_configure_accounts'] = 'Hazi Accounts';
+$lang['smtp_destination_domains'] = 'Destination Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP Okpu logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer bụ ghara ịdị irè.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'The hostname na domain ntọala nwere ike ahazi na Mail Ntọala ngwa.';
+$lang['smtp_maximum_message_size'] = 'Oke Message Size';
+$lang['smtp_port_invalid'] = 'Port bụ ghara ịdị irè.';
+$lang['smtp_recipient_mapping_invalid'] = 'Nnata nkewa bụ ghara ịdị irè.';
+$lang['smtp_relay_host_invalid'] = 'Ọsọ agba anọ usu bụ ghara ịdị irè.';
+$lang['smtp_relay_host'] = 'ọsọ agba anọ Host';
+$lang['smtp_server_invalid'] = 'SMTP Server bụ ghara ịdị irè.';
+$lang['smtp_smtp_authentication'] = 'SMTP nyocha';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Tụkwasịrị Obi netwọk bụ ghara ịdị irè.';
+$lang['smtp_trusted_networks'] = 'tụkwasịrị Obi Networks';
+$lang['smtp_user_policies'] = 'User atumatu';
