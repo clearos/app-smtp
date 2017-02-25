c6bb60e618ce913bdd4cce3f052a37ae8c40e4b3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zh/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = '为了使SMTP服务器中的所有功能，账户系统必须初始化';
+$lang['smtp_app_description'] = 'SMTP服务器提供了一个接收和发送邮件服务器以及邮件转发和SMTP认证功能。';
+$lang['smtp_app_name'] = 'SMTP服务器';
+$lang['smtp_block_plaintext'] = '块纯文本密码';
+$lang['smtp_configure_accounts'] = '配置帐户';
+$lang['smtp_destination_domains'] = '目标域';
+$lang['smtp_failed_login_detection'] = 'SMTP登录失败';
+$lang['smtp_mail_disclaimer_invalid'] = '邮件声明是无效的。';
+$lang['smtp_mail_forwarding'] = '邮件转发';
+$lang['smtp_mail_settings_in_mail_app_message'] = '主机名和域名设置可以在邮件设置应用程序进行配置。';
+$lang['smtp_maximum_message_size'] = '最大邮件大小';
+$lang['smtp_port_invalid'] = '港是无效的。';
+$lang['smtp_recipient_mapping_invalid'] = '收件人映射无效。';
+$lang['smtp_relay_host_invalid'] = '中继主机是无效的。';
+$lang['smtp_relay_host'] = '中继主机';
+$lang['smtp_server_invalid'] = 'SMTP服务器是无效的。';
+$lang['smtp_smtp_authentication'] = 'SMTP认证';
+$lang['smtp_smtp_server'] = 'SMTP服务器';
+$lang['smtp_target_server'] = '目标服务器';
+$lang['smtp_trusted_network_invalid'] = '信任的网络是无效的。';
+$lang['smtp_trusted_networks'] = '可信网络';
+$lang['smtp_user_policies'] = '用户策略';
