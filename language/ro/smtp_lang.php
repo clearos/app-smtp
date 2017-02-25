c6c628f08e2e2254d305356dfff5bdc3a756e2d4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:20:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ro/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Pentru a permite toate caracteristicile din serverul SMTP, sistemul de conturi trebuie să fie inițializat';
+$lang['smtp_app_description'] = 'Serverul SMTP oferă un server de intrare și de ieșire e-mail precum și de redirecționare a corespondenței și autentificare SMTP caracteristici.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Bloc text simplu parole';
+$lang['smtp_configure_accounts'] = 'Conturi Configurați';
+$lang['smtp_destination_domains'] = 'Domenii de destinație';
+$lang['smtp_failed_login_detection'] = 'Conectări SMTP nu a reușit';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail prin disclaimer este nevalid.';
+$lang['smtp_mail_forwarding'] = 'redirecționării';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Setările pentru numele de gazdă și de domeniu pot fi configurate în aplicația Setări Mail.';
+$lang['smtp_maximum_message_size'] = 'Dimensiune maximă Mesaj';
+$lang['smtp_port_invalid'] = 'Portul este nevalid.';
+$lang['smtp_recipient_mapping_invalid'] = 'cartografiere destinatar este nevalid.';
+$lang['smtp_relay_host_invalid'] = 'Releu de gazdă este nevalid.';
+$lang['smtp_relay_host'] = 'gazdă releu';
+$lang['smtp_server_invalid'] = 'Serverul SMTP este nevalid.';
+$lang['smtp_smtp_authentication'] = 'autentificare SMTP';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server-țintă';
+$lang['smtp_trusted_network_invalid'] = 'rețea de încredere este nevalid.';
+$lang['smtp_trusted_networks'] = 'Rețele de încredere';
+$lang['smtp_user_policies'] = 'Politicile de utilizator';
