b5531c2b183c77e9184283537e5d2b834e1e0c57
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eu/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Ordena SMTP zerbitzaria ezaugarri guztiak izan ditzan, kontuak sistema hasieratu behar';
+$lang['smtp_app_description'] = 'SMTP zerbitzaria, sarrerako eta irteerako posta-zerbitzari bat baita birbidaltzea eta SMTP autentifikazioa funtzio ditu.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Passwords';
+$lang['smtp_configure_accounts'] = 'Konfiguratu kontuak';
+$lang['smtp_destination_domains'] = 'Helmuga domeinuak';
+$lang['smtp_failed_login_detection'] = 'SMTP huts egindako sarrerak';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer baliogabea da.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'hostname eta domeinu ezarpenak Mail ezarpenak aplikazio konfigura daitezke.';
+$lang['smtp_maximum_message_size'] = 'Gehienezko Mezuaren tamaina';
+$lang['smtp_port_invalid'] = 'Port baliogabea da.';
+$lang['smtp_recipient_mapping_invalid'] = 'Hartzailearen mapping baliogabea da.';
+$lang['smtp_relay_host_invalid'] = 'Relay ostalari baliogabea da.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server baliogabea da.';
+$lang['smtp_smtp_authentication'] = 'SMTP autentifikazioa';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Xede Server';
+$lang['smtp_trusted_network_invalid'] = 'Konfiantzazko sarea baliogabea da.';
+$lang['smtp_trusted_networks'] = 'Konfiantzazko Sareak';
+$lang['smtp_user_policies'] = 'Erabiltzaile Politikak';
