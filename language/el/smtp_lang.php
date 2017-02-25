406424a4396162111448f047b30d83b2ff8c3d4d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/el/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Για να μπορέσουν όλα τα χαρακτηριστικά του SMTP Server, το σύστημα λογαριασμών πρέπει να προετοιμαστεί';
+$lang['smtp_app_description'] = 'Ο διακομιστής SMTP παρέχει μια εισερχόμενη και εξερχόμενη διακομιστή αλληλογραφίας, καθώς και προώθησης της αλληλογραφίας και τον έλεγχο ταυτότητας SMTP χαρακτηριστικά.';
+$lang['smtp_app_name'] = 'SMTP διακομιστή';
+$lang['smtp_block_plaintext'] = 'Αποκλεισμός απλού κειμένου κωδικούς πρόσβασης';
+$lang['smtp_configure_accounts'] = 'Διαμόρφωση Λογαριασμοί';
+$lang['smtp_destination_domains'] = 'Domains προορισμός';
+$lang['smtp_failed_login_detection'] = 'SMTP απέτυχε Συνδέσεις';
+$lang['smtp_mail_disclaimer_invalid'] = 'Αποποίηση ευθυνών Mail είναι έγκυρη.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Οι ρυθμίσεις hostname και το domain μπορεί να ρυθμιστεί στην εφαρμογή Ρυθμίσεις Mail.';
+$lang['smtp_maximum_message_size'] = 'Μέγιστο μέγεθος μηνύματος';
+$lang['smtp_port_invalid'] = 'Θύρα δεν είναι έγκυρο.';
+$lang['smtp_recipient_mapping_invalid'] = 'χαρτογράφηση παραλήπτη δεν είναι έγκυρο.';
+$lang['smtp_relay_host_invalid'] = 'Ρελέ υποδοχής δεν είναι έγκυρη.';
+$lang['smtp_relay_host'] = 'Αναμετάδοση κεντρικού υπολογιστή';
+$lang['smtp_server_invalid'] = 'SMTP διακομιστή δεν είναι έγκυρο.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP διακομιστή';
+$lang['smtp_target_server'] = 'στόχου διακομιστή';
+$lang['smtp_trusted_network_invalid'] = 'Αξιόπιστο δίκτυο δεν είναι έγκυρο.';
+$lang['smtp_trusted_networks'] = 'Αξιόπιστες Δίκτυα';
+$lang['smtp_user_policies'] = 'Πολιτικές χρήστη';
