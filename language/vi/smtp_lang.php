37f0d687da1d91d6e6557a3cb576060c4fd4e2ca
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:42 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/vi/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Để kích hoạt tất cả các tính năng trong các máy chủ SMTP, hệ thống tài khoản phải được khởi tạo';
+$lang['smtp_app_description'] = 'SMTP Server cung cấp một máy chủ thư đến và đi cũng như chuyển tiếp thư và xác thực SMTP tính năng.';
+$lang['smtp_app_name'] = 'SMTP server';
+$lang['smtp_block_plaintext'] = 'Khối Plain-Text Mật khẩu';
+$lang['smtp_configure_accounts'] = 'Tài khoản Configure';
+$lang['smtp_destination_domains'] = 'Tên miền điểm đến';
+$lang['smtp_failed_login_detection'] = 'SMTP Không Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Thư từ chối trách nhiệm là không hợp lệ.';
+$lang['smtp_mail_forwarding'] = 'thư Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Các thiết lập hostname và domain có thể được cấu hình trong ứng dụng Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Tối đa tin nhắn Kích';
+$lang['smtp_port_invalid'] = 'Port là không hợp lệ.';
+$lang['smtp_recipient_mapping_invalid'] = 'lập bản đồ nhận không hợp lệ.';
+$lang['smtp_relay_host_invalid'] = 'Rơ le máy chủ không hợp lệ.';
+$lang['smtp_relay_host'] = 'Tiếp chủ';
+$lang['smtp_server_invalid'] = 'SMTP Server là không hợp lệ.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP server';
+$lang['smtp_target_server'] = 'máy chủ mục tiêu';
+$lang['smtp_trusted_network_invalid'] = 'mạng tin cậy là không hợp lệ.';
+$lang['smtp_trusted_networks'] = 'Mạng tin cậy';
+$lang['smtp_user_policies'] = 'Chính sách người dùng';
