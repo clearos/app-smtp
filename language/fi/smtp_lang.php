6be10cfeb3c545808d6afa2609c4a35fda830cfc
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fi/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Jotta kaikki toiminnot SMTP-palvelin, tilit järjestelmä on alustettu';
+$lang['smtp_app_description'] = 'SMTP-palvelin tarjoaa saapuvan ja lähtevän postin palvelimen sekä postinohjaus- ja SMTP autentikointi ominaisuuksia.';
+$lang['smtp_app_name'] = 'SMTP-palvelin';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Salasanat';
+$lang['smtp_configure_accounts'] = 'Määrittele tunnukset';
+$lang['smtp_destination_domains'] = 'Kohde Verkkotunnukset';
+$lang['smtp_failed_login_detection'] = 'SMTP epäonnistui Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail vastuuvapauslauseke on virheellinen.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Hostname ja verkkotunnuksen asetukset voidaan konfiguroida Mail-asetukset-sovelluksessa.';
+$lang['smtp_maximum_message_size'] = 'Viestin enimmäiskoko';
+$lang['smtp_port_invalid'] = 'Port on virheellinen.';
+$lang['smtp_recipient_mapping_invalid'] = 'Vastaanottaja kartoitus on virheellinen.';
+$lang['smtp_relay_host_invalid'] = 'Relay isäntä on virheellinen.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP-palvelin ei kelpaa.';
+$lang['smtp_smtp_authentication'] = 'SMTP-todennus';
+$lang['smtp_smtp_server'] = 'SMTP-palvelin';
+$lang['smtp_target_server'] = 'palvelin';
+$lang['smtp_trusted_network_invalid'] = 'Luotettu verkko on virheellinen.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'Käyttäjä säännöt';
