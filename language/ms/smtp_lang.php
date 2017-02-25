71da58af1b4c8cd98362359915028fc27a8ee46f
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:32 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ms/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Untuk membolehkan semua ciri-ciri dalam Pelayan SMTP, sistem akaun mesti dimulakan';
+$lang['smtp_app_description'] = 'The SMTP Server menyediakan pelayan mel masuk dan keluar serta penghantar mel dan pengesahan SMTP ciri-ciri.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Kata laluan';
+$lang['smtp_configure_accounts'] = 'Akaun Configure';
+$lang['smtp_destination_domains'] = 'Domain Destinasi';
+$lang['smtp_failed_login_detection'] = 'SMTP Gagal Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail penafian tidak sah.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Tetapan nama hos dan domain boleh ditetapkan didalam aplikasi Mel Tetapan.';
+$lang['smtp_maximum_message_size'] = 'Saiz Maksimum Mesej';
+$lang['smtp_port_invalid'] = 'Port adalah tidak sah.';
+$lang['smtp_recipient_mapping_invalid'] = 'Penerima pemetaan tidak sah.';
+$lang['smtp_relay_host_invalid'] = 'Relay tuan rumah adalah tidak sah.';
+$lang['smtp_relay_host'] = 'Hos geganti';
+$lang['smtp_server_invalid'] = 'SMTP Server adalah tidak sah.';
+$lang['smtp_smtp_authentication'] = 'Pengesahan SMTP';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server target';
+$lang['smtp_trusted_network_invalid'] = 'rangkaian Dipercayai tidak sah.';
+$lang['smtp_trusted_networks'] = 'Networks dipercayai';
+$lang['smtp_user_policies'] = 'Polisi pengguna';
