eeec25711988ac93fd47d07a6eb73fec851d1535
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:26 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ta/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP சர்வர் அனைத்து அம்சங்கள் செயல்படுத்த பொருட்டு, கணக்குகள் அமைப்பு துவக்க வேண்டும்';
+$lang['smtp_app_description'] = 'SMTP சர்வர் ஒரு உள்வரும் மற்றும் வெளிச்செல்லும் அஞ்சல் சர்வர் அத்துடன் மெயில் பகிர்தல் மற்றும் SMTP அங்கீகரிப்பு அம்சங்களை வழங்குகிறது.';
+$lang['smtp_app_name'] = 'SMTP சர்வர்';
+$lang['smtp_block_plaintext'] = 'பிளாக் எளிய உரை கடவுச்சொற்கள்';
+$lang['smtp_configure_accounts'] = 'கட்டமை கணக்குகள்';
+$lang['smtp_destination_domains'] = 'இலக்கு களங்கள்';
+$lang['smtp_failed_login_detection'] = 'SMTP, தோல்வியடைந்தது உள் அனுமதிகள்';
+$lang['smtp_mail_disclaimer_invalid'] = 'மின்னஞ்சல் பொறுப்பாகாமை தவறானது.';
+$lang['smtp_mail_forwarding'] = 'அஞ்சல் பகிர்தல்';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'புரவலன் பெயர் மற்றும் டொமைன் அமைப்புகள் அஞ்சல் அமைப்புகள் பயன்பாட்டை கட்டமைக்க முடியும்.';
+$lang['smtp_maximum_message_size'] = 'செய்தி அளவைவிட';
+$lang['smtp_port_invalid'] = 'போர்ட் தவறானது.';
+$lang['smtp_recipient_mapping_invalid'] = 'பெறுநரின் மேப்பிங் தவறானது.';
+$lang['smtp_relay_host_invalid'] = 'ரிலே புரவலன் தவறானது.';
+$lang['smtp_relay_host'] = 'ரிலே புரவலன்';
+$lang['smtp_server_invalid'] = 'SMTP சர்வர் தவறானது.';
+$lang['smtp_smtp_authentication'] = 'SMTP அத்தாட்சிப்படுத்தல்';
+$lang['smtp_smtp_server'] = 'SMTP சர்வர்';
+$lang['smtp_target_server'] = 'இலக்கு சர்வர்';
+$lang['smtp_trusted_network_invalid'] = 'நம்பகமான பிணைய தவறானது.';
+$lang['smtp_trusted_networks'] = 'நம்பகமான நெட்வொர்க்ஸ்';
+$lang['smtp_user_policies'] = 'பயனர் கொள்கைகள்';
