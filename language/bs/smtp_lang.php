aac036474d0b09c12f7c34cc49a34dff1f744517
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:49 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bs/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Kako bi se omogućilo svim funkcijama u SMTP Server, sistem računa se mora pokrenuti';
+$lang['smtp_app_description'] = 'SMTP Server pruža ulazne i izlazne mail server, kao i mail prosljeđivanje i autentifikaciju SMTP funkcije.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Blok tekstualne lozinke';
+$lang['smtp_configure_accounts'] = 'Konfiguracija računa';
+$lang['smtp_destination_domains'] = 'Destinacija domena';
+$lang['smtp_failed_login_detection'] = 'SMTP neuspješni upisi';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail odricanje je nevažeća.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Podešavanja hostname i domena može biti konfiguriran u aplikaciji Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Maksimalna veličina poruke';
+$lang['smtp_port_invalid'] = 'Luka je nevažeći.';
+$lang['smtp_recipient_mapping_invalid'] = 'mapiranje Primalac je nevažeća.';
+$lang['smtp_relay_host_invalid'] = 'Relej domaćin je nevažeća.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server je nevažeća.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Pouzdana mreža je nevažeća.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'korisnik politike';
