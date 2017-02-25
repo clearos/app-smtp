2f25b0409d8bb4400709a4430c34083b8d2db769
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ko/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP 서버의 모든 기능을 활성화하기 위해, 계정 시스템을 초기화해야';
+$lang['smtp_app_description'] = 'SMTP 서버는 수신 및 발신 메일 서버뿐만 아니라 메일 전달 및 SMTP 인증 기능을 제공합니다.';
+$lang['smtp_app_name'] = 'SMTP 서버';
+$lang['smtp_block_plaintext'] = '블록 일반 텍스트 암호';
+$lang['smtp_configure_accounts'] = '계정 구성';
+$lang['smtp_destination_domains'] = '대상 도메인';
+$lang['smtp_failed_login_detection'] = 'SMTP 로그인 실패';
+$lang['smtp_mail_disclaimer_invalid'] = '메일 면책 조항이 유효하지 않습니다.';
+$lang['smtp_mail_forwarding'] = '메일 전달';
+$lang['smtp_mail_settings_in_mail_app_message'] = '호스트 이름 및 도메인 설정은 메일 설정 응용 프로그램에서 구성 할 수 있습니다.';
+$lang['smtp_maximum_message_size'] = '최대 메시지 크기';
+$lang['smtp_port_invalid'] = '포트가 잘못되었습니다.';
+$lang['smtp_recipient_mapping_invalid'] = '받는 사람 매핑이 잘못되었습니다.';
+$lang['smtp_relay_host_invalid'] = '릴레이 호스트가 잘못되었습니다.';
+$lang['smtp_relay_host'] = '릴레이 호스트';
+$lang['smtp_server_invalid'] = 'SMTP 서버가 유효하지 않습니다.';
+$lang['smtp_smtp_authentication'] = 'SMTP 인증';
+$lang['smtp_smtp_server'] = 'SMTP 서버';
+$lang['smtp_target_server'] = '대상 서버';
+$lang['smtp_trusted_network_invalid'] = '신뢰할 수있는 네트워크가 잘못되었습니다.';
+$lang['smtp_trusted_networks'] = '신뢰할 수있는 네트워크';
+$lang['smtp_user_policies'] = '사용자 정책';
