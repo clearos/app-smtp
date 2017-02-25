417c8aec13ae422883d8e0ad3394a4dd043f39c6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:27 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/fr/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Afin de permettre à toutes les fonctionnalités du serveur SMTP, le système des comptes doit être initialisé';
+$lang['smtp_app_description'] = 'Le serveur SMTP fournit un serveur de courrier entrant et sortant ainsi que le courrier expédition et d&#39;authentification SMTP fonctionnalités.';
+$lang['smtp_app_name'] = 'serveur SMTP';
+$lang['smtp_block_plaintext'] = 'Bloc Plain-texte des mots de passe';
+$lang['smtp_configure_accounts'] = 'Configurer les comptes';
+$lang['smtp_destination_domains'] = 'Domaines de destination';
+$lang['smtp_failed_login_detection'] = 'SMTP Échec Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'disclaimer Mail est invalide.';
+$lang['smtp_mail_forwarding'] = 'réexpédition du courrier';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Les paramètres de nom d&#39;hôte et de domaine peuvent être configurés dans l&#39;application Paramètres de messagerie.';
+$lang['smtp_maximum_message_size'] = 'Message Taille maximale';
+$lang['smtp_port_invalid'] = 'Port est invalide.';
+$lang['smtp_recipient_mapping_invalid'] = 'la cartographie du bénéficiaire est invalide.';
+$lang['smtp_relay_host_invalid'] = 'Hôte relais est invalide.';
+$lang['smtp_relay_host'] = 'hôte relais';
+$lang['smtp_server_invalid'] = 'Serveur SMTP est invalide.';
+$lang['smtp_smtp_authentication'] = 'authentification SMTP';
+$lang['smtp_smtp_server'] = 'serveur SMTP';
+$lang['smtp_target_server'] = 'serveur cible';
+$lang['smtp_trusted_network_invalid'] = 'Réseau de confiance est invalide.';
+$lang['smtp_trusted_networks'] = 'Réseaux de confiance';
+$lang['smtp_user_policies'] = 'Politiques de l&#39;utilisateur';
