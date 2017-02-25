ac13588287463bf39149e3ed0d04a2fa77fd3188
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:46 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/cs/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'S cílem umožnit všechny funkce serveru SMTP, musí být systém účtů inicializaci';
+$lang['smtp_app_description'] = 'SMTP Server poskytuje příchozí a odchozí e-mailový server, stejně jako přeposílání pošty a autentizace SMTP funkce.';
+$lang['smtp_app_name'] = 'SMTP server';
+$lang['smtp_block_plaintext'] = 'Blok prostého textu hesla';
+$lang['smtp_configure_accounts'] = 'Konfigurace účty';
+$lang['smtp_destination_domains'] = 'cílových domén';
+$lang['smtp_failed_login_detection'] = 'SMTP se nezdařilo přihlášení';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer je neplatný.';
+$lang['smtp_mail_forwarding'] = 'Přeposílání pošty';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Nastavení hostname a domény lze nastavit v aplikaci Nastavení pošty.';
+$lang['smtp_maximum_message_size'] = 'Maximální velikost zprávy';
+$lang['smtp_port_invalid'] = 'Port je neplatný.';
+$lang['smtp_recipient_mapping_invalid'] = 'Mapování příjemce je neplatný.';
+$lang['smtp_relay_host_invalid'] = 'hostitel relé je neplatný.';
+$lang['smtp_relay_host'] = 'relé hostitele';
+$lang['smtp_server_invalid'] = 'SMTP server je neplatný.';
+$lang['smtp_smtp_authentication'] = 'ověření SMTP';
+$lang['smtp_smtp_server'] = 'SMTP server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Důvěryhodná síť je neplatný.';
+$lang['smtp_trusted_networks'] = 'důvěryhodné sítě';
+$lang['smtp_user_policies'] = 'Zásady uživatelských';
