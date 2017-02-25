22192caf85f7fc24888a43e389c0a7b6c5f46234
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:13 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/la/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Ut in omnibus liniamenta SMTP ut Servo, ratio est ratio initialized';
+$lang['smtp_app_description'] = 'Servo in SMTP suggero an advenientis et exitu mail server cum bene sicut transmittuntur SMTP authenticas et features.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Plain Text Block-Passwords';
+$lang['smtp_configure_accounts'] = 'Configure Ratio';
+$lang['smtp_destination_domains'] = 'Destination Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP robur Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail Disclaimer est invalidum.';
+$lang['smtp_mail_forwarding'] = 'transmittuntur';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Dominationem et occasus possumus hostname Cursuales felis in app.';
+$lang['smtp_maximum_message_size'] = 'Maximum Size Nuntius';
+$lang['smtp_port_invalid'] = 'Port est invalidum.';
+$lang['smtp_recipient_mapping_invalid'] = 'Recipient mapping est invalidum.';
+$lang['smtp_relay_host_invalid'] = 'Nullam nulla virtus est.';
+$lang['smtp_relay_host'] = 'Nullam Host';
+$lang['smtp_server_invalid'] = 'Servo SMTP est invalidum.';
+$lang['smtp_smtp_authentication'] = 'SMTP authenticitate';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Trusted network est invalidum.';
+$lang['smtp_trusted_networks'] = 'Networks Trusted';
+$lang['smtp_user_policies'] = 'User Politiae';
