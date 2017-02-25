f7347b9d84b5f2401778f032c4ce8a7c568da648
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:20:30 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/pl/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'W celu umożliwienia wszystkie funkcje w serwerze SMTP, system kont muszą być inicjowane';
+$lang['smtp_app_description'] = 'Serwer SMTP zapewnia przychodzące i wychodzące serwera pocztowego, jak również przekazywania poczty elektronicznej i uwierzytelniania SMTP możliwości.';
+$lang['smtp_app_name'] = 'Serwer SMTP';
+$lang['smtp_block_plaintext'] = 'Blok tekstowe Hasła';
+$lang['smtp_configure_accounts'] = 'konfigurowanie kont';
+$lang['smtp_destination_domains'] = 'Wylot Domeny';
+$lang['smtp_failed_login_detection'] = 'SMTP Failed logowania';
+$lang['smtp_mail_disclaimer_invalid'] = 'Poczta zrzeczenie jest nieprawidłowy.';
+$lang['smtp_mail_forwarding'] = 'Przekazywanie poczty';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Nazwa hosta i domeny ustawienia mogą zostać skonfigurowane w aplikacji Ustawienia poczty.';
+$lang['smtp_maximum_message_size'] = 'Maksymalny rozmiar wiadomości';
+$lang['smtp_port_invalid'] = 'Port jest nieprawidłowy.';
+$lang['smtp_recipient_mapping_invalid'] = 'Mapowanie Odbiorca jest nieprawidłowy.';
+$lang['smtp_relay_host_invalid'] = 'hosta Przekaźnik jest nieprawidłowy.';
+$lang['smtp_relay_host'] = 'Przekaźnik Host';
+$lang['smtp_server_invalid'] = 'Serwer SMTP jest nieprawidłowy.';
+$lang['smtp_smtp_authentication'] = 'Autoryzacja SMTP';
+$lang['smtp_smtp_server'] = 'Serwer SMTP';
+$lang['smtp_target_server'] = 'Serwer docelowy';
+$lang['smtp_trusted_network_invalid'] = 'Sieć zaufana jest nieprawidłowy.';
+$lang['smtp_trusted_networks'] = 'Zaufane Networks';
+$lang['smtp_user_policies'] = 'Zasady użytkownika';
