5f4cd469939aaf49b1c4026a0c725cf3a7b6602a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:21:53 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/es/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Con el fin de activar todas las funciones del servidor SMTP, el sistema de cuentas debe ser inicializado';
+$lang['smtp_app_description'] = 'El servidor SMTP proporciona un servidor de correo entrante y saliente, así como funciones de reenvío de correo electrónico y autenticación SMTP.';
+$lang['smtp_app_name'] = 'servidor SMTP';
+$lang['smtp_block_plaintext'] = 'Bloque de texto sin formato contraseñas';
+$lang['smtp_configure_accounts'] = 'Configurar cuentas';
+$lang['smtp_destination_domains'] = 'Dominios de destino';
+$lang['smtp_failed_login_detection'] = 'Inicios de sesión SMTP Error';
+$lang['smtp_mail_disclaimer_invalid'] = 'descargo de responsabilidad electrónico no es válida.';
+$lang['smtp_mail_forwarding'] = 'Reenvío de correo';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Los ajustes de máquina o dominio se pueden configurar en la aplicación Configuración de Mensajes.';
+$lang['smtp_maximum_message_size'] = 'Tamaño máximo del mensaje';
+$lang['smtp_port_invalid'] = 'El puerto no es válido.';
+$lang['smtp_recipient_mapping_invalid'] = 'mapeo destinatario no es válido.';
+$lang['smtp_relay_host_invalid'] = 'Host de retransmisión no es válido.';
+$lang['smtp_relay_host'] = 'Host de retransmisión';
+$lang['smtp_server_invalid'] = 'Servidor SMTP no es válido.';
+$lang['smtp_smtp_authentication'] = 'autenticación SMTP';
+$lang['smtp_smtp_server'] = 'servidor SMTP';
+$lang['smtp_target_server'] = 'servidor de destino';
+$lang['smtp_trusted_network_invalid'] = 'Red de confianza no es válido.';
+$lang['smtp_trusted_networks'] = 'Redes de confianza';
+$lang['smtp_user_policies'] = 'Directivas de usuario';
