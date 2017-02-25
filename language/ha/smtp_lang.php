3967c157359895b0c941c897d650eabeb8b6d83b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ha/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Domin taimaka dukan fasali a cikin SMTP Server, da asusun tsarin dole ne a initialized';
+$lang['smtp_app_description'] = 'The SMTP Server samar da wani mai shigowa da kuma mai fita mail server kazalika mail isar da kuma SMTP Tantance kalmar sirri fasali.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Bayyana-Text Kalmomin sirri';
+$lang['smtp_configure_accounts'] = 'Sanya Accounts';
+$lang['smtp_destination_domains'] = 'manufa Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP nasarar logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer ba daidai ba ne.';
+$lang['smtp_mail_forwarding'] = 'Mail Mikawa';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'The sunan mai masauki da kuma yankin saituna za a iya kaga a Mail Saituna app.';
+$lang['smtp_maximum_message_size'] = 'Maximum Message Size';
+$lang['smtp_port_invalid'] = 'Port ba daidai ba ne.';
+$lang['smtp_recipient_mapping_invalid'] = 'Karba zana taswira ba shi da inganci.';
+$lang['smtp_relay_host_invalid'] = 'Relay rundunar ba daidai ba ne.';
+$lang['smtp_relay_host'] = 'Relay watsa shiri';
+$lang['smtp_server_invalid'] = 'SMTP Server ba daidai ba ne.';
+$lang['smtp_smtp_authentication'] = 'SMTP Gasktawa';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Amintattun cibiyar sadarwa ba daidai ba ne.';
+$lang['smtp_trusted_networks'] = 'Amintattun Networks';
+$lang['smtp_user_policies'] = 'User Asiri';
