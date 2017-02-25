3439934f042321e095aaf6d051d605c83db76224
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:57 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bg/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'С цел да се даде възможност на всички функции в SMTP сървър, системата на сметките трябва да се инициализира';
+$lang['smtp_app_description'] = 'Сървър SMTP осигурява входящата и изходяща поща на сървъра, както и препращане на поща и SMTP Authentication функции.';
+$lang['smtp_app_name'] = 'SMTP сървър';
+$lang['smtp_block_plaintext'] = 'Блок обикновен текст пароли';
+$lang['smtp_configure_accounts'] = 'Конфигуриране на сметки';
+$lang['smtp_destination_domains'] = 'Дестинация домейни';
+$lang['smtp_failed_login_detection'] = 'SMTP Неуспешно Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail опровержение е невалиден.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Настройките на име на хост и домейн могат да бъдат конфигурирани в приложението Настройки на пощата на.';
+$lang['smtp_maximum_message_size'] = 'Максимален размер на ЛС';
+$lang['smtp_port_invalid'] = 'Port е невалиден.';
+$lang['smtp_recipient_mapping_invalid'] = 'Получател картографиране е невалиден.';
+$lang['smtp_relay_host_invalid'] = 'Relay домакин е невалиден.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP сървър е невалиден.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP сървър';
+$lang['smtp_target_server'] = 'Целева Сървър';
+$lang['smtp_trusted_network_invalid'] = 'Trusted мрежа е невалиден.';
+$lang['smtp_trusted_networks'] = 'Надеждни Networks';
+$lang['smtp_user_policies'] = 'политики на потребителя';
