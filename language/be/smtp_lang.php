424697bb2ffbb1c69f32c988293d3d2eb242af31
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/be/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Для таго каб ўключыць усе функцыі ў SMTP-сервер, сістэма рахункаў павінна быць ініцыялізаваны';
+$lang['smtp_app_description'] = 'Сервер SMTP забяспечвае які ўваходзіць і выходны паштовы сервер, а таксама перасылка пошты і аўтэнтыфікацыі SMTP функцыі.';
+$lang['smtp_app_name'] = 'SMTP-сервер';
+$lang['smtp_block_plaintext'] = 'Блок простыя тэкставыя паролі';
+$lang['smtp_configure_accounts'] = 'Налада уліковых запісаў';
+$lang['smtp_destination_domains'] = 'напрамак Дамены';
+$lang['smtp_failed_login_detection'] = 'SMTP Памылка лагін';
+$lang['smtp_mail_disclaimer_invalid'] = 'Пошта адмова ад адказнасці з&#39;яўляецца несапраўдным.';
+$lang['smtp_mail_forwarding'] = 'пераадрасацыя пошты';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Параметры хаста і дамен можна наладзіць у наладах прыкладання Mail.';
+$lang['smtp_maximum_message_size'] = 'Максімальны памер паведамлення';
+$lang['smtp_port_invalid'] = 'Порт з&#39;яўляецца несапраўдным.';
+$lang['smtp_recipient_mapping_invalid'] = 'Адлюстраванне атрымальнікаў з&#39;яўляецца несапраўдным.';
+$lang['smtp_relay_host_invalid'] = 'Рэле хаста несапраўднае.';
+$lang['smtp_relay_host'] = 'рэле хаста';
+$lang['smtp_server_invalid'] = 'Сервер SMTP з&#39;яўляецца недапушчальным.';
+$lang['smtp_smtp_authentication'] = 'аўтэнтыфікацыя SMTP';
+$lang['smtp_smtp_server'] = 'SMTP-сервер';
+$lang['smtp_target_server'] = 'мэтавай сервер';
+$lang['smtp_trusted_network_invalid'] = 'Давераныя сеткі з&#39;яўляецца недапушчальным.';
+$lang['smtp_trusted_networks'] = 'давераныя сеткі';
+$lang['smtp_user_policies'] = 'Правілы для карыстальнікаў';
