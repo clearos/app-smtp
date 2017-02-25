ce5d05412b7db073ad8bbe4fb1f6faa1375f60da
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:00 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/gu/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'ક્રમમાં SMTP સર્વર તમામ લક્ષણો સક્રિય કરવા માટે, એકાઉન્ટ્સ સિસ્ટમ આરંભ થવો જ જોઈએ';
+$lang['smtp_app_description'] = 'SMTP સર્વર ઇનકમિંગ અને આઉટગોઇંગ મેલ સર્વર તેમજ મેલ ફોરવર્ડ કરવા અને SMTP સત્તાધિકરણ સુવિધાઓ પૂરી પાડે છે.';
+$lang['smtp_app_name'] = 'SMTP સર્વર';
+$lang['smtp_block_plaintext'] = 'બ્લોક સાદા લખાણ પાસવર્ડો';
+$lang['smtp_configure_accounts'] = 'રૂપરેખાંકિત એકાઉન્ટ્સ';
+$lang['smtp_destination_domains'] = 'લક્ષ્યસ્થાન ડોમેન્સ';
+$lang['smtp_failed_login_detection'] = 'SMTP નિષ્ફળ પ્રવેશો';
+$lang['smtp_mail_disclaimer_invalid'] = 'મેઇલ ડિસક્લેમર અમાન્ય છે.';
+$lang['smtp_mail_forwarding'] = 'મેઇલ આગળ ધપાવવાની';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'યજમાનનામ અને ડોમેન સેટિંગ્સ મેઇલ સેટિંગ્સ એપ્લિકેશન રૂપરેખાંકિત કરી શકાય છે.';
+$lang['smtp_maximum_message_size'] = 'મહત્તમ સંદેશ કદ';
+$lang['smtp_port_invalid'] = 'પોર્ટ અમાન્ય છે.';
+$lang['smtp_recipient_mapping_invalid'] = 'પ્રાપ્તિકર્તા મેપિંગ અમાન્ય છે.';
+$lang['smtp_relay_host_invalid'] = 'રિલે હોસ્ટ અમાન્ય છે.';
+$lang['smtp_relay_host'] = 'રિલે યજમાન';
+$lang['smtp_server_invalid'] = 'SMTP સર્વર અમાન્ય છે.';
+$lang['smtp_smtp_authentication'] = 'SMTP સત્તાધિકરણ';
+$lang['smtp_smtp_server'] = 'SMTP સર્વર';
+$lang['smtp_target_server'] = 'લક્ષ્ય સર્વર';
+$lang['smtp_trusted_network_invalid'] = 'વિશ્વાસુ નેટવર્ક અમાન્ય છે.';
+$lang['smtp_trusted_networks'] = 'વિશ્વાસુ નેટવર્ક્સ';
+$lang['smtp_user_policies'] = 'વપરાશકર્તા નીતિઓ';
