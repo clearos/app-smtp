cd6a9b30e3e15a155deb3029bde24c488d20815c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:25 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ur/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP سرور میں تمام خصوصیات کو چالو کرنے کے لئے، اکاؤنٹس نظام سے initialized کیا جائے ضروری ہے';
+$lang['smtp_app_description'] = 'SMTP سرور میں ایک آنے والے اور سبکدوش ہونے والے میل سرور کے طور پر بھی میل بڑھانے اور SMTP تصدیق خصوصیات فراہم.';
+$lang['smtp_app_name'] = 'SMTP سرور';
+$lang['smtp_block_plaintext'] = 'بلاک سادہ ٹیکسٹ پاسورڈس';
+$lang['smtp_configure_accounts'] = 'ترتیب دیں اکاؤنٹس';
+$lang['smtp_destination_domains'] = 'ہدف ڈومین';
+$lang['smtp_failed_login_detection'] = 'SMTP ناکام لاگ ان';
+$lang['smtp_mail_disclaimer_invalid'] = 'میل کلیمر غلط ہے.';
+$lang['smtp_mail_forwarding'] = 'میل فارورڈنگ';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'میزبان نام اور ڈومین ترتیبات میل کی ترتیبات اپلی کیشن میں تشکیل کر سکتے ہیں.';
+$lang['smtp_maximum_message_size'] = 'زیادہ سے زیادہ پیغام سائز';
+$lang['smtp_port_invalid'] = 'پورٹ غلط ہے.';
+$lang['smtp_recipient_mapping_invalid'] = 'وصول کنندہ تعریفیں درست نہیں ہے.';
+$lang['smtp_relay_host_invalid'] = 'ریلے کے میزبان غلط ہے.';
+$lang['smtp_relay_host'] = 'ریلے ہوسٹ';
+$lang['smtp_server_invalid'] = 'SMTP سرور غلط ہے.';
+$lang['smtp_smtp_authentication'] = 'SMTP تصدیق';
+$lang['smtp_smtp_server'] = 'SMTP سرور';
+$lang['smtp_target_server'] = 'ٹارگٹ سرور';
+$lang['smtp_trusted_network_invalid'] = 'قابل اعتماد نیٹ ورک غلط ہے.';
+$lang['smtp_trusted_networks'] = 'بھروسہ مند نیٹ ورکس';
+$lang['smtp_user_policies'] = 'یوزر کی پالیسیاں';
