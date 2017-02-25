16c4ac93e9bcdd8a5df04b8a150830444c8466e9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:21 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ny/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Pofuna kuthandiza mbali zonse mu Server SMTP, dongosolo nkhani ayenera initialized';
+$lang['smtp_app_description'] = 'The SMTP Server apereke ukubwera ndi ochezeka makalata makina komanso makalata kutumiza ndi kutsimikizika SMTP mbali.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text achinsinsi';
+$lang['smtp_configure_accounts'] = 'Nkhani sintha';
+$lang['smtp_destination_domains'] = 'kopita madambwe';
+$lang['smtp_failed_login_detection'] = 'SMTP Chalephera Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail chandalama ndi opanda pake.';
+$lang['smtp_mail_forwarding'] = 'Mail kutumiza';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'The zoikamo hostname ndipo ankalamulira akhoza kukhazikitsidwa mu app Mail Zikhazikiko.';
+$lang['smtp_maximum_message_size'] = 'Pazipita Uthenga Kukula';
+$lang['smtp_port_invalid'] = 'Port ndi opanda pake.';
+$lang['smtp_recipient_mapping_invalid'] = 'Wolandira sanjira ndi opanda pake.';
+$lang['smtp_relay_host_invalid'] = 'Kulandirana khamu ili udzakhala osazindikilika.';
+$lang['smtp_relay_host'] = 'kulandirana khamu';
+$lang['smtp_server_invalid'] = 'SMTP Server ndi opanda pake.';
+$lang['smtp_smtp_authentication'] = 'SMTP kutsimikizika';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server chandamale';
+$lang['smtp_trusted_network_invalid'] = 'zopezera anakhulupirira ndi opanda pake.';
+$lang['smtp_trusted_networks'] = 'Networks anakhulupirira';
+$lang['smtp_user_policies'] = 'Ndondomeko wosuta';
