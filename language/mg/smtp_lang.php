4f7ee0558dcbee053bdb5f4efd7022882cd6a6fd
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:44 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mg/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Mba hahafahan&#39;ny rehetra ny endri-javatra teo amin&#39;ny SMTP Server, ny fitantarana rafitra tsy maintsy initialized';
+$lang['smtp_app_description'] = 'Ny SMTP manome Narahi-Server sy sariaka namany Sary mpizara ary koa ny taratasy fanamarinana SMTP nanatitra sy ny endri-javatra.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text ny tenimiafina';
+$lang['smtp_configure_accounts'] = 'Tefeo ny Kaonty';
+$lang['smtp_destination_domains'] = 'Destination Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP Tsy nahomby Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer dia tsy manan-kery.';
+$lang['smtp_mail_forwarding'] = 'Mail nanatitra';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Ny hostname sy ny sehatra toe-javatra azo voaendrika ao amin&#39;ny Mail Settings fampiharana.';
+$lang['smtp_maximum_message_size'] = 'Maximum Message Size';
+$lang['smtp_port_invalid'] = 'Port dia tsy manan-kery.';
+$lang['smtp_recipient_mapping_invalid'] = 'Mpandray sarintany dia tsy manan-kery.';
+$lang['smtp_relay_host_invalid'] = 'Handefasana miaramila dia tsy manan-kery.';
+$lang['smtp_relay_host'] = 'fampitàna Host';
+$lang['smtp_server_invalid'] = 'SMTP Server dia tsy manan-kery.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Itokisana Network dia tsy manan-kery.';
+$lang['smtp_trusted_networks'] = 'itokisana Networks';
+$lang['smtp_user_policies'] = 'User Policies';
