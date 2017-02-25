84dcc006e2262b976510006fcde031d032e0ebc3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:17 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/az/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP Server bütün xüsusiyyətləri təmin etmək üçün, hesabı sistemi başlatılmış lazımdır';
+$lang['smtp_app_description'] = 'SMTP Server gələn və çıxan mail server, eləcə də poçt forwarding və SMTP identifikasiyası xüsusiyyətləri təmin edir.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block düz mətn Parollar';
+$lang['smtp_configure_accounts'] = 'Yapılandır Accounts';
+$lang['smtp_destination_domains'] = 'Destination Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP Uğursuz Girişler';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer yanlışdır.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'hostname və domen ayarları Mail Ayarlar app konfiqurasiya edilə bilər.';
+$lang['smtp_maximum_message_size'] = 'Maximum Message Size';
+$lang['smtp_port_invalid'] = 'Port yanlışdır.';
+$lang['smtp_recipient_mapping_invalid'] = 'Alan Xəritəçəkmə yanlışdır.';
+$lang['smtp_relay_host_invalid'] = 'Relay host yanlışdır.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server yanlışdır.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Trusted şəbəkə yanlışdır.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'İstifadəçi Siyasətlər';
