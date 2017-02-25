01b99f8dd8245bb60a01a11404f7bf06f7430e07
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hy/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Որպեսզի բոլոր հատկանիշները է SMTP Server, որ հաշիվները համակարգը պետք է initialized';
+$lang['smtp_app_description'] = 'The SMTP Server ապահովում է մուտքային եւ ելքային փոստի սերվեր, ինչպես նաեւ mail շեղում եւ SMTP իսկությունը առանձնահատկություններ.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Արգելափակել Plain-տեքստային գաղտնաբառերը';
+$lang['smtp_configure_accounts'] = 'կարգավորել հաշիվներ';
+$lang['smtp_destination_domains'] = 'վերջնակետ Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP ձախողված';
+$lang['smtp_mail_disclaimer_invalid'] = 'Փոստ Ուրացում է անվավեր:';
+$lang['smtp_mail_forwarding'] = 'Փոստ Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Այն Հոսթի անվանում եւ դոմենային կայանքները կարող է կազմաձեւվել Փոստ Settings ծրագրի.';
+$lang['smtp_maximum_message_size'] = 'Առավելագույն չափը հաղորդագրություն';
+$lang['smtp_port_invalid'] = 'Port է անվավեր:';
+$lang['smtp_recipient_mapping_invalid'] = 'Ստացող քարտեզագրման է անվավեր:';
+$lang['smtp_relay_host_invalid'] = 'Relay հյուրընկալող անվավեր է:';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server է անվավեր:';
+$lang['smtp_smtp_authentication'] = 'SMTP Նույնականացման';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Թիրախային Server';
+$lang['smtp_trusted_network_invalid'] = 'Վստահել ցանցը անվավեր:';
+$lang['smtp_trusted_networks'] = 'վստահելի ցանցեր';
+$lang['smtp_user_policies'] = 'User քաղաքականություն';
