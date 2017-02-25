29a9621244ded66389691fa24c26adcc8c19ed7b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:23 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ja/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTPサーバーのすべての機能を有効にするためには、アカウントシステムを初期化する必要があります';
+$lang['smtp_app_description'] = 'SMTPサーバーは、受信と送信メールサーバーだけでなく、メール転送とSMTP認証機能を提供します。';
+$lang['smtp_app_name'] = 'SMTPサーバー';
+$lang['smtp_block_plaintext'] = 'ブロックプレーンテキストのパスワード';
+$lang['smtp_configure_accounts'] = 'アカウントを設定';
+$lang['smtp_destination_domains'] = '宛先ドメイン';
+$lang['smtp_failed_login_detection'] = 'SMTP失敗したログイン';
+$lang['smtp_mail_disclaimer_invalid'] = 'メール免責事項は無効です。';
+$lang['smtp_mail_forwarding'] = 'メール転送';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ホスト名とドメインの設定は、メール設定アプリで設定することができます。';
+$lang['smtp_maximum_message_size'] = '最大メッセージサイズ';
+$lang['smtp_port_invalid'] = 'ポートは無効です。';
+$lang['smtp_recipient_mapping_invalid'] = '受信者のマッピングは無効です。';
+$lang['smtp_relay_host_invalid'] = 'リレーホストが無効です。';
+$lang['smtp_relay_host'] = '中継ホスト';
+$lang['smtp_server_invalid'] = 'SMTPサーバーは無効です。';
+$lang['smtp_smtp_authentication'] = 'SMTP認証';
+$lang['smtp_smtp_server'] = 'SMTPサーバー';
+$lang['smtp_target_server'] = 'ターゲットサーバ';
+$lang['smtp_trusted_network_invalid'] = 'トラステッド・ネットワークは無効です。';
+$lang['smtp_trusted_networks'] = 'トラステッドネットワーク';
+$lang['smtp_user_policies'] = 'ユーザーポリシー';
