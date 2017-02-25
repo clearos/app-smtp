be6d8cfb9a15581de36d2eb83347fadd9028dbee
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:21:03 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sr/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Да би се омогућило све функције у СМТП Сервер, систем рачуни морају бити иницијализована';
+$lang['smtp_app_description'] = 'СМТП сервер даје долазни и одлазни маил сервер као и пошта шпедиције и СМТП потврду идентитета функције.';
+$lang['smtp_app_name'] = 'СМТП сервер';
+$lang['smtp_block_plaintext'] = 'Блок Обичан-Текст лозинке';
+$lang['smtp_configure_accounts'] = 'Подешавање налога';
+$lang['smtp_destination_domains'] = 'Дестинатион Домени';
+$lang['smtp_failed_login_detection'] = 'СМТП Фаилед Пријаве';
+$lang['smtp_mail_disclaimer_invalid'] = 'Маил одрицање је неважећи.';
+$lang['smtp_mail_forwarding'] = 'маил Прослеђивање';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Подешавања хостнаме и домена може се конфигурисати у апликацији Маил Сеттингс.';
+$lang['smtp_maximum_message_size'] = 'Максимална величина порука';
+$lang['smtp_port_invalid'] = 'Порт је неважећи.';
+$lang['smtp_recipient_mapping_invalid'] = 'мапирање прималац је неважећи.';
+$lang['smtp_relay_host_invalid'] = 'Релеј домаћин је неважећи.';
+$lang['smtp_relay_host'] = 'релеј Домаћин';
+$lang['smtp_server_invalid'] = 'СМТП сервер је неважећи.';
+$lang['smtp_smtp_authentication'] = 'СМТП Аутхентицатион';
+$lang['smtp_smtp_server'] = 'СМТП сервер';
+$lang['smtp_target_server'] = 'таргет сервер';
+$lang['smtp_trusted_network_invalid'] = 'Трустед мреже је неважећи.';
+$lang['smtp_trusted_networks'] = 'проверених мреже';
+$lang['smtp_user_policies'] = 'усер Политика';
