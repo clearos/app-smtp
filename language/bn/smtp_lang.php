5be98ef1e8d718afdacf8da147c4a9b86f25bfb3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:41 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/bn/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'অর্ডার SMTP সার্ভার সমস্ত বৈশিষ্ট্য সক্রিয় করার জন্য, অ্যাকাউন্ট সিস্টেম সক্রিয়া করা আবশ্যক';
+$lang['smtp_app_description'] = 'SMTP সার্ভার একটি অন্তর্মুখী ও বহির্মুখী মেইল ​​সার্ভার সেইসাথে মেইল ​​ফরওয়ার্ডিং এবং SMTP এর প্রমাণীকরণ বৈশিষ্ট্য উপলব্ধ করা হয়.';
+$lang['smtp_app_name'] = 'SMTP সার্ভার';
+$lang['smtp_block_plaintext'] = 'ব্লক প্লেইন টেক্সট পাসওয়ার্ড';
+$lang['smtp_configure_accounts'] = 'কনফিগার অ্যাকাউন্টস';
+$lang['smtp_destination_domains'] = 'গন্তব্য ডোমেইন';
+$lang['smtp_failed_login_detection'] = 'SMTP এর ব্যর্থ লগইন';
+$lang['smtp_mail_disclaimer_invalid'] = 'মেল দাবিত্যাগ অবৈধ.';
+$lang['smtp_mail_forwarding'] = 'মেইল ফরোয়ার্ড';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'হোস্টনেম এবং ডোমেইন সেটিংস মেইল ​​সেটিংস অ্যাপ্লিকেশন কনফিগার করা যাবে.';
+$lang['smtp_maximum_message_size'] = 'সর্বোচ্চ বার্তা সাইজ';
+$lang['smtp_port_invalid'] = 'পোর্ট অবৈধ.';
+$lang['smtp_recipient_mapping_invalid'] = 'প্রাপক ম্যাপিং অবৈধ.';
+$lang['smtp_relay_host_invalid'] = 'রিলে হোস্ট অবৈধ.';
+$lang['smtp_relay_host'] = 'রিলে হোস্ট';
+$lang['smtp_server_invalid'] = 'SMTP সার্ভার অবৈধ.';
+$lang['smtp_smtp_authentication'] = 'SMTP এর প্রমাণীকরণ';
+$lang['smtp_smtp_server'] = 'SMTP সার্ভার';
+$lang['smtp_target_server'] = 'লক্ষ্য সার্ভার';
+$lang['smtp_trusted_network_invalid'] = 'বিশ্বস্ত নেটওয়ার্ক অবৈধ.';
+$lang['smtp_trusted_networks'] = 'বিশ্বস্ত নেটওয়ার্ক';
+$lang['smtp_user_policies'] = 'ব্যবহারকারীর নীতিসমূহ';
