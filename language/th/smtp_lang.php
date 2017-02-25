10c2075776bd23138152c6c2d07fec7a1a3b0db3
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:51 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/th/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'เพื่อที่จะเปิดใช้งานคุณลักษณะทั้งหมดที่อยู่ในเซิร์ฟเวอร์ SMTP ระบบบัญชีจะต้องเริ่มต้น';
+$lang['smtp_app_description'] = 'เซิร์ฟเวอร์ SMTP ให้เซิร์ฟเวอร์จดหมายขาเข้าและขาออกเช่นเดียวกับการส่งต่อจดหมายและคุณสมบัติการตรวจสอบ SMTP';
+$lang['smtp_app_name'] = 'เซิร์ฟเวอร์ SMTP';
+$lang['smtp_block_plaintext'] = 'บล็อกข้อความธรรมดารหัสผ่าน';
+$lang['smtp_configure_accounts'] = 'บัญชีกำหนดค่า';
+$lang['smtp_destination_domains'] = 'โดเมนปลายทาง';
+$lang['smtp_failed_login_detection'] = 'SMTP ไม่สามารถเข้าสู่ระบบ';
+$lang['smtp_mail_disclaimer_invalid'] = 'จดหมายข้อจำกัดความรับผิดชอบไม่ถูกต้อง';
+$lang['smtp_mail_forwarding'] = 'ส่งต่อจดหมาย';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'การตั้งค่าชื่อโฮสต์และโดเมนสามารถกำหนดค่าในแอปตั้งค่าจดหมาย';
+$lang['smtp_maximum_message_size'] = 'ขนาดของข้อความสูงสุด';
+$lang['smtp_port_invalid'] = 'พอร์ตไม่ถูกต้อง';
+$lang['smtp_recipient_mapping_invalid'] = 'การทำแผนที่ผู้รับไม่ถูกต้อง';
+$lang['smtp_relay_host_invalid'] = 'โฮสต์ Relay ไม่ถูกต้อง';
+$lang['smtp_relay_host'] = 'รีเลย์โฮสต์';
+$lang['smtp_server_invalid'] = 'เซิร์ฟเวอร์ SMTP ไม่ถูกต้อง';
+$lang['smtp_smtp_authentication'] = 'ค่า SMTP Authentication';
+$lang['smtp_smtp_server'] = 'เซิร์ฟเวอร์ SMTP';
+$lang['smtp_target_server'] = 'เซิร์ฟเวอร์เป้าหมาย';
+$lang['smtp_trusted_network_invalid'] = 'เครือข่ายที่เชื่อถือไม่ถูกต้อง';
+$lang['smtp_trusted_networks'] = 'เครือข่ายที่เชื่อถือได้';
+$lang['smtp_user_policies'] = 'นโยบายของผู้ใช้';
