b2c02ef43296217fb5796ed361b1c1c6b040d800
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:48 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/km/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'ក្នុងគោលបំណងដើម្បីបើកលក្ខណៈពិសេសទាំងអស់នៅក្នុងម៉ាស៊ីនបម្រើ SMTP របស់ប្រព័ន្ធគណនីត្រូវតែចាប់ផ្តើម';
+$lang['smtp_app_description'] = 'ម៉ាស៊ីនបម្រើ SMTP នេះបានផ្ដល់នូវម៉ាស៊ីនបម្រើសំបុត្រចូលនិងចេញដូចជាលក្ខណៈពិសេសសំបុត្របញ្ជូនបន្តនិងការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវ SMTP ។';
+$lang['smtp_app_name'] = 'ម៉ាស៊ីនបម្រើ SMTP';
+$lang['smtp_block_plaintext'] = 'ពាក្យសម្ងាត់អត្ថបទធម្មតាប្លុក';
+$lang['smtp_configure_accounts'] = 'កំណត់រចនាសម្ព័ន្ធគណនី';
+$lang['smtp_destination_domains'] = 'ដែនគោលដៅ';
+$lang['smtp_failed_login_detection'] = 'ចូល SMTP ដែលបានបរាជ័យក្នុង';
+$lang['smtp_mail_disclaimer_invalid'] = 'សេចក្តីអះអាងសំបុត្រមិនត្រឹមត្រូវ។';
+$lang['smtp_mail_forwarding'] = 'សំបុត្របញ្ជូនបន្ត';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'ការកំណត់ឈ្មោះម៉ាស៊ីននិងដែនអាចកំណត់រចនាសម្ព័ន្ធការកំណត់សំបុត្រក្នុងកម្មវិធី។';
+$lang['smtp_maximum_message_size'] = 'ទំហំសារអតិបរមា';
+$lang['smtp_port_invalid'] = 'កំពង់ផែមិនត្រឹមត្រូវ។';
+$lang['smtp_recipient_mapping_invalid'] = 'ការគូសផែនទីអ្នកទទួលមិនត្រឹមត្រូវ។';
+$lang['smtp_relay_host_invalid'] = 'ម៉ាស៊ីនបញ្ជូនតមិនត្រឹមត្រូវ។';
+$lang['smtp_relay_host'] = 'ម៉ាស៊ីនបញ្ជូនត';
+$lang['smtp_server_invalid'] = 'ម៉ាស៊ីនបម្រើ SMTP មិនត្រឹមត្រូវ។';
+$lang['smtp_smtp_authentication'] = 'ការផ្ទៀងផ្ទាត់ភាពត្រឹមត្រូវរបស់ SMTP';
+$lang['smtp_smtp_server'] = 'ម៉ាស៊ីនបម្រើ SMTP';
+$lang['smtp_target_server'] = 'ម៉ាស៊ីនបម្រើសគោលដៅ';
+$lang['smtp_trusted_network_invalid'] = 'បណ្តាញដែលជឿទុកចិត្តមិនត្រឹមត្រូវ។';
+$lang['smtp_trusted_networks'] = 'បណ្តាញដែលបានជឿទុកចិត្ត';
+$lang['smtp_user_policies'] = 'គោលនយោបាយរបស់អ្នកប្រើ';
