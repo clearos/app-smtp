ff8ad150f8dd864c8246e53f97da708ed9e8c871
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/tg/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Бо мақсади фароҳам тамоми хусусиятҳо дар SMTP сервер, аз системаи ҳисобҳои бояд initialized мешавад';
+$lang['smtp_app_description'] = 'Дар SMTP Server як сервери почта воридотӣ ва содиротӣ, инчунин хусусиятҳои интиқоли почта ва аслӣ будани SMTP таъмин менамояд.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Блоки оддӣ-матн Паролҳо';
+$lang['smtp_configure_accounts'] = 'ҳисоб Танзими';
+$lang['smtp_destination_domains'] = 'Доирањои Самт';
+$lang['smtp_failed_login_detection'] = 'SMTP муяссар рамзњои';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail Радди нодуруст аст.';
+$lang['smtp_mail_forwarding'] = 'Mail экспедитор';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Гузоришҳои номи мизбони ва домен мумкин аст, дар барнома танзимоти почтаи электронӣ танзим карда.';
+$lang['smtp_maximum_message_size'] = 'Message Андозаи ниҳоии';
+$lang['smtp_port_invalid'] = 'Порт нодуруст аст.';
+$lang['smtp_recipient_mapping_invalid'] = 'харитасозии гирандаи нодуруст аст.';
+$lang['smtp_relay_host_invalid'] = 'мизбон мепайвандад нодуруст аст.';
+$lang['smtp_relay_host'] = 'Host мепайвандад';
+$lang['smtp_server_invalid'] = 'SMTP Server нодуруст аст.';
+$lang['smtp_smtp_authentication'] = 'SMTP Таъйид';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server мақсаднок';
+$lang['smtp_trusted_network_invalid'] = 'Шабакаи боваринок нодуруст аст.';
+$lang['smtp_trusted_networks'] = 'Шабакаҳои боэътимод';
+$lang['smtp_user_policies'] = 'сиёсати корбар';
