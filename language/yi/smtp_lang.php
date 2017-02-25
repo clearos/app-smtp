aa56676c22154f118ede7811dc7b9f95f49ffce9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:23:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/yi/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'אין סדר צו געבן אַלע די פֿעיִקייטן אין דער סמטפּ סערווער, די אַקאַונץ סיסטעם מוזן זיין יניטיאַליזעד';
+$lang['smtp_app_description'] = 'די סמטפּ סערווירער גיט אַ ינקאַמינג און אַוטגאָוינג פּאָסט סערווער ווי ווויל ווי פּאָסט Forwarding און סמטפּ אָטענטאַקיישאַן פֿעיִקייטן.';
+$lang['smtp_app_name'] = 'סמטפּ סערווירער';
+$lang['smtp_block_plaintext'] = 'בלאַק קלאָר-טעקסט פּאַססוואָרדס';
+$lang['smtp_configure_accounts'] = 'configure אַקקאָונץ';
+$lang['smtp_destination_domains'] = 'דעסטינאַטיאָן דאָומיינז';
+$lang['smtp_failed_login_detection'] = 'סמטפּ אַנדערש לאָגינס';
+$lang['smtp_mail_disclaimer_invalid'] = 'מעיל אָפּלייקענונג איז פאַרקריפּלט.';
+$lang['smtp_mail_forwarding'] = 'מעיל Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'די האָסטנאַמע און פעלד סעטטינגס קענען זיין קאָנפיגורעד אין די פּאָסט סעטטינגס אַפּ.';
+$lang['smtp_maximum_message_size'] = 'מאַקסימום אַרטיקל גרייס';
+$lang['smtp_port_invalid'] = 'פּאָרט איז פאַרקריפּלט.';
+$lang['smtp_recipient_mapping_invalid'] = 'באַקומער מאַפּינג איז פאַרקריפּלט.';
+$lang['smtp_relay_host_invalid'] = 'ריליי באַלעבאָס איז פאַרקריפּלט.';
+$lang['smtp_relay_host'] = 'ריליי האָסט';
+$lang['smtp_server_invalid'] = 'סמטפּ סערווירער איז פאַרקריפּלט.';
+$lang['smtp_smtp_authentication'] = 'סמטפּ אַוטהענטיקאַטיאָן';
+$lang['smtp_smtp_server'] = 'סמטפּ סערווירער';
+$lang['smtp_target_server'] = 'ציל סערווירער';
+$lang['smtp_trusted_network_invalid'] = 'טרוסטעד נעץ איז פאַרקריפּלט.';
+$lang['smtp_trusted_networks'] = 'טרוסטעד נעטוואָרקס';
+$lang['smtp_user_policies'] = 'באַניצער פּאָליסיעס';
