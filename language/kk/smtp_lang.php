4ddd12136659f95ebf0e4e59cd032702d0dc65bb
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/kk/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'SMTP сервері ішіндегі барлық мүмкіндіктерді қосу үшін, шоттар жүйесі енгізуге болады';
+$lang['smtp_app_description'] = 'SMTP сервер кіріс және шығыс пошта серверін сондай-ақ пошта жіберу және SMTP аутентификация мүмкіндіктерін қамтамасыз етеді.';
+$lang['smtp_app_name'] = 'SMTP сервер';
+$lang['smtp_block_plaintext'] = 'Блок кәдімгі мәтін Құпия сөздер';
+$lang['smtp_configure_accounts'] = 'Тіркелгілер конфигурациялау';
+$lang['smtp_destination_domains'] = 'Destination Домендер';
+$lang['smtp_failed_login_detection'] = 'SMTP Сәтсіз логиніңізбен кіріңіз';
+$lang['smtp_mail_disclaimer_invalid'] = 'Пошта Жауапкершіліктен бас тарту жарамсыз болып табылады.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Хост атауы және домен параметрлері пошта параметрлері қолданбасында конфигурациялауға болады.';
+$lang['smtp_maximum_message_size'] = 'Ең үлкен хабар өлшемі';
+$lang['smtp_port_invalid'] = 'Порт жарамсыз болып табылады.';
+$lang['smtp_recipient_mapping_invalid'] = 'Алушы картографиялық жарамсыз болып табылады.';
+$lang['smtp_relay_host_invalid'] = 'Relay хост жарамсыз болып табылады.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP сервер жарамсыз болып табылады.';
+$lang['smtp_smtp_authentication'] = 'SMTP Аутентификация';
+$lang['smtp_smtp_server'] = 'SMTP сервер';
+$lang['smtp_target_server'] = 'мақсатты сервер';
+$lang['smtp_trusted_network_invalid'] = 'Сенімді желілік жарамсыз болып табылады.';
+$lang['smtp_trusted_networks'] = 'Сенімді Networks';
+$lang['smtp_user_policies'] = 'пайдаланушы саясаты';
