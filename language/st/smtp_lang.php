2d8f4602cdd7c61c0823a96ec8e55cea240c9e5c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:21:45 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/st/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'E le hore a etsa hore litšobotsi tsohle ka Server SMTP, tsamaiso litlaleho lokela ho initialized';
+$lang['smtp_app_description'] = 'The SMTP Server e fana ka e kenang le botsoalle haholoanyane poso seva hammoho le poso fetisetse le netefatso SMTP ditshebetso tse.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text diphasewete';
+$lang['smtp_configure_accounts'] = 'Litlaleho fetola ha';
+$lang['smtp_destination_domains'] = 'eang teng domains';
+$lang['smtp_failed_login_detection'] = 'SMTP hlōleha Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail ikgololo ha o sebetse.';
+$lang['smtp_mail_forwarding'] = 'Mail fetisetse';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Di-setting ka hostname le domain name e ka di kentswe ka app e Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Palo e kahodimodimo Message Size';
+$lang['smtp_port_invalid'] = 'Port ha o sebetse.';
+$lang['smtp_recipient_mapping_invalid'] = 'Moamohedi tseba likarolo ha o sebetse.';
+$lang['smtp_relay_host_invalid'] = 'Lesokoana neng a amohetse moeti ha o sebetse.';
+$lang['smtp_relay_host'] = 'lesokoana ngata';
+$lang['smtp_server_invalid'] = 'SMTP Server ha o sebetse.';
+$lang['smtp_smtp_authentication'] = 'SMTP ya netefatso';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server shebiloeng';
+$lang['smtp_trusted_network_invalid'] = 'netweke eo u mo tšepang ha o sebetse.';
+$lang['smtp_trusted_networks'] = 'Networks eo u mo tšepang';
+$lang['smtp_user_policies'] = 'Maano User';
