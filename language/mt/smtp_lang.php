d9c7c248873bd950d53d89d28e17b49f2c41d86d
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:40 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/mt/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Sabiex tippermetti l-karatteristiċi kollha fil-Server SMTP, is-sistema kontijiet għandhom jiġu initialized';
+$lang['smtp_app_description'] = 'Il Server SMTP jipprovdi server posta dieħla u ħierġa kif ukoll posta twassil u awtentikazzjoni SMTP karatteristiċi.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Blokk Plain-Test passwords';
+$lang['smtp_configure_accounts'] = 'Kontijiet Kkonfigurat';
+$lang['smtp_destination_domains'] = 'Setturi destinazzjoni';
+$lang['smtp_failed_login_detection'] = 'SMTP rnexxielhomx logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'caħda ta &#39;responsabbiltà mail huwa invalidu.';
+$lang['smtp_mail_forwarding'] = 'mail Tressiq';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Il hostname u dominju settings jistgħu jiġu kkonfigurati fil-app Mail Settings.';
+$lang['smtp_maximum_message_size'] = 'Daqs massimu Messaġġ';
+$lang['smtp_port_invalid'] = 'Port huwa invalidu.';
+$lang['smtp_recipient_mapping_invalid'] = 'immappjar reċipjent huwa invalidu.';
+$lang['smtp_relay_host_invalid'] = 'Relay ospitanti huwa invalidu.';
+$lang['smtp_relay_host'] = 'relay Ospitanti';
+$lang['smtp_server_invalid'] = 'SMTP Server huwa invalidu.';
+$lang['smtp_smtp_authentication'] = 'SMTP Awtentikazzjoni';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server mira';
+$lang['smtp_trusted_network_invalid'] = 'network ta &#39;fiduċja huwa invalidu.';
+$lang['smtp_trusted_networks'] = 'Netwerks fiduċja';
+$lang['smtp_user_policies'] = 'Politiki utent';
