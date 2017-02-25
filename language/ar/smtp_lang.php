2b68ce315a49d34e2fb4d1e08ccf125581d1f3f6
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:13:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ar/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'من أجل تمكين جميع الميزات الموجودة في خادم SMTP، لا بد من تهيئة نظام الحسابات';
+$lang['smtp_app_description'] = 'يوفر خادم SMTP خادم البريد الوارد والصادر وكذلك إعادة توجيه البريد ومصادقة SMTP الميزات.';
+$lang['smtp_app_name'] = 'خادم SMTP';
+$lang['smtp_block_plaintext'] = 'كتلة عادي نص كلمات المرور';
+$lang['smtp_configure_accounts'] = 'الحسابات تكوين';
+$lang['smtp_destination_domains'] = 'مجالات المقصد';
+$lang['smtp_failed_login_detection'] = 'محدودة SMTP فشل';
+$lang['smtp_mail_disclaimer_invalid'] = 'تنويه الإلكتروني غير صالح.';
+$lang['smtp_mail_forwarding'] = 'إعادة توجيه البريد';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'يمكن تكوين إعدادات المضيف والمجال في التطبيق إعدادات البريد.';
+$lang['smtp_maximum_message_size'] = 'رسالة الحجم الأقصى';
+$lang['smtp_port_invalid'] = 'ميناء غير صالح.';
+$lang['smtp_recipient_mapping_invalid'] = 'رسم الخرائط مستلم غير صالح.';
+$lang['smtp_relay_host_invalid'] = 'المضيف الترحيل غير صالح.';
+$lang['smtp_relay_host'] = 'المضيف التتابع';
+$lang['smtp_server_invalid'] = 'خادم SMTP غير صالح.';
+$lang['smtp_smtp_authentication'] = 'مصادقة SMTP';
+$lang['smtp_smtp_server'] = 'خادم SMTP';
+$lang['smtp_target_server'] = 'خادم الهدف';
+$lang['smtp_trusted_network_invalid'] = 'شبكة موثوق به غير صالحة.';
+$lang['smtp_trusted_networks'] = 'شبكات موثوق به';
+$lang['smtp_user_policies'] = 'سياسات المستخدم';
