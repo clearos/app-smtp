59a31986436b9a84b71fac31828cbbd13b19cc4e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:18 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sv/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'För att alla funktioner i SMTP-server, måste systemet konton initieras';
+$lang['smtp_app_description'] = 'SMTP-servern tillhandahåller en inkommande och utgående e-postserver samt vidarebefordran av post och SMTP autentiseringsfunktioner.';
+$lang['smtp_app_name'] = 'SMTP-server';
+$lang['smtp_block_plaintext'] = 'Block klartext lösenord';
+$lang['smtp_configure_accounts'] = 'Ställ in konton';
+$lang['smtp_destination_domains'] = 'destinations domäner';
+$lang['smtp_failed_login_detection'] = 'SMTP misslyckades inloggningar';
+$lang['smtp_mail_disclaimer_invalid'] = 'Post disclaimer är ogiltig.';
+$lang['smtp_mail_forwarding'] = 'post Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'De värdnamn och domäninställningar kan konfigureras i appen E-postinställningar.';
+$lang['smtp_maximum_message_size'] = 'Maxstorlek';
+$lang['smtp_port_invalid'] = 'Port är ogiltig.';
+$lang['smtp_recipient_mapping_invalid'] = 'Mottagare kartläggning är ogiltig.';
+$lang['smtp_relay_host_invalid'] = 'Relay värd är ogiltig.';
+$lang['smtp_relay_host'] = 'relä~~POS=TRUNC värd~~POS=TRUNC';
+$lang['smtp_server_invalid'] = 'SMTP-server är ogiltig.';
+$lang['smtp_smtp_authentication'] = 'SMTP-autentisering';
+$lang['smtp_smtp_server'] = 'SMTP-server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Tillförlitligt nätverk är ogiltig.';
+$lang['smtp_trusted_networks'] = 'betrodda Networks';
+$lang['smtp_user_policies'] = 'Användarregler';
