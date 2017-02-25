9abfc9e610ba702945259b3b5c43fa0fbed2e8c9
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:17:07 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/is/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Til þess að gera allar aðgerðir í SMTP Server, reikningar kerfið verður að frumstilla';
+$lang['smtp_app_description'] = 'SMTP Server veitir pósts framreiðslumaður auk mail áfram og SMTP staðfesting lögun.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Lykilorð';
+$lang['smtp_configure_accounts'] = 'Stilla reikningar';
+$lang['smtp_destination_domains'] = 'Destination Lén';
+$lang['smtp_failed_login_detection'] = 'SMTP mistókst innskráningar';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail fyrirvari er ógilt.';
+$lang['smtp_mail_forwarding'] = 'mail Áframsending';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Hostname og lénsstillingarnar er hægt að stilla í pósti Stillingar forrita.';
+$lang['smtp_maximum_message_size'] = 'Hámarks Message Size';
+$lang['smtp_port_invalid'] = 'Port er ógilt.';
+$lang['smtp_recipient_mapping_invalid'] = 'Viðtakandinn kortlagning er ógilt.';
+$lang['smtp_relay_host_invalid'] = 'Relay gestgjafi er ógilt.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server er ógilt.';
+$lang['smtp_smtp_authentication'] = 'SMTP Staðfesting';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Öruggar net er ógilt.';
+$lang['smtp_trusted_networks'] = 'Öruggar Networks';
+$lang['smtp_user_policies'] = 'User Reglur';
