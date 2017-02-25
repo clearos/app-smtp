054bc8036cbed7ee20f44088450632aad2b72a2b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:54 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/da/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'For at gøre det muligt for alle funktionerne i SMTP-server, skal regnskabssystem initialiseres';
+$lang['smtp_app_description'] = 'SMTP Server giver et indgående og udgående mailserver samt mail videresendelse og SMTP authentication funktioner.';
+$lang['smtp_app_name'] = 'SMTP-server';
+$lang['smtp_block_plaintext'] = 'Block almindelig tekst Passwords';
+$lang['smtp_configure_accounts'] = 'Konfigurer konti';
+$lang['smtp_destination_domains'] = 'Destination domæner';
+$lang['smtp_failed_login_detection'] = 'SMTP mislykkedes Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail ansvarsfraskrivelse er ugyldig.';
+$lang['smtp_mail_forwarding'] = 'Mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Indstillingerne for værtsnavn og domæne kan konfigureres i app mail-indstillinger.';
+$lang['smtp_maximum_message_size'] = 'Maksimal Message Size';
+$lang['smtp_port_invalid'] = 'Port er ugyldig.';
+$lang['smtp_recipient_mapping_invalid'] = 'Modtager mapping er ugyldig.';
+$lang['smtp_relay_host_invalid'] = 'Relæ vært er ugyldig.';
+$lang['smtp_relay_host'] = 'Relævært';
+$lang['smtp_server_invalid'] = 'SMTP-server er ugyldig.';
+$lang['smtp_smtp_authentication'] = 'SMTP-godkendelse';
+$lang['smtp_smtp_server'] = 'SMTP-server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Pålideligt netværk er ugyldig.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'Bruger Politikker';
