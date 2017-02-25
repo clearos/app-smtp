e56826392ab0ff7845fbbfd26c066c21713f37c1
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:14:05 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/my/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'အ SMTP Server ကိုအပေါငျးတို့သ features တွေကိုဖွင့်ဖို့အမိန့်ထဲမှာ, အကောင့်အသစ်များ၏ system ကိုစတင်ဖို့ရဦးမည်';
+$lang['smtp_app_description'] = 'အဆိုပါ SMTP Server ကိုတစ်ဦးအဝင်နဲ့အထွက်မေးလ်ဆာဗာအဖြစ်ကောင်းစွာအဖြစ်မေးလ် forwarding နှင့် SMTP authentication ကို features တွေကိုပေးစွမ်းသည်။';
+$lang['smtp_app_name'] = 'SMTP Server ကို';
+$lang['smtp_block_plaintext'] = 'block လွင်ပြင်-စာသား Passwords များကို';
+$lang['smtp_configure_accounts'] = 'Accounts ကို configure';
+$lang['smtp_destination_domains'] = 'destination ဒိုမိန်း';
+$lang['smtp_failed_login_detection'] = 'SMTP မအောင်မြင်ခဲ့ပါ login';
+$lang['smtp_mail_disclaimer_invalid'] = 'မေးလ်မသက်ဆိုင်ကြောင်းရှင်းလင်းချက်မမှန်ကန်ဘူး။';
+$lang['smtp_mail_forwarding'] = 'မေးလ် Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'အဆိုပါ hostname နှင့်ဒိုမိန်း settings ကိုမေးလ်က Settings app ထဲမှာ configured နိုင်ပါသည်။';
+$lang['smtp_maximum_message_size'] = 'အများဆုံးကို Message Size ကို';
+$lang['smtp_port_invalid'] = 'ဆိပ်ကမ်းမမှန်ကန်ဘူး။';
+$lang['smtp_recipient_mapping_invalid'] = 'လက်ခံမဲ့သူကိုမြေပုံမမှန်ကန်ဘူး။';
+$lang['smtp_relay_host_invalid'] = 'relay အိမ်ရှင်မမှန်ကန်ဘူး။';
+$lang['smtp_relay_host'] = 'relay Host က';
+$lang['smtp_server_invalid'] = 'SMTP Server ကိုမမှန်ကန်ဘူး။';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP Server ကို';
+$lang['smtp_target_server'] = 'target က Server ကို';
+$lang['smtp_trusted_network_invalid'] = 'အသိမှတ်ပြုကွန်ယက်ကိုမမှန်ကန်ဘူး။';
+$lang['smtp_trusted_networks'] = 'အသိမှတ်ပြုကွန်ယက်';
+$lang['smtp_user_policies'] = 'အသုံးပြုသူမူဝါဒများ';
