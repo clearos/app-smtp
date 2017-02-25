c758ccac3c4743a653bdab8c60961fb9ebb2adf4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:33 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/hu/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Annak érdekében, hogy minden funkciót az SMTP szerver, a beszámoló rendszerben kell inicializálni';
+$lang['smtp_app_description'] = 'Az SMTP szerver ad a bejövő és kimenő mail szerver, valamint mail továbbítás és az SMTP hitelesítési funkciók.';
+$lang['smtp_app_name'] = 'SMTP szerver';
+$lang['smtp_block_plaintext'] = 'Blokk sima szöveges jelszavak';
+$lang['smtp_configure_accounts'] = 'Beállítás számlák';
+$lang['smtp_destination_domains'] = 'Destination Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP sikertelen bejelentkezés';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail jognyilatkozat érvénytelen.';
+$lang['smtp_mail_forwarding'] = 'mail továbbítás';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'A gépnév és tartomány beállítás konfigurálható a Mail Settings alkalmazást.';
+$lang['smtp_maximum_message_size'] = 'Üzenet maximális mérete';
+$lang['smtp_port_invalid'] = 'Port érvénytelen.';
+$lang['smtp_recipient_mapping_invalid'] = 'A címzett mapping érvénytelen.';
+$lang['smtp_relay_host_invalid'] = 'Továbbító állomás érvénytelen.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP szerver érvénytelen.';
+$lang['smtp_smtp_authentication'] = 'SMTP hitelesítés';
+$lang['smtp_smtp_server'] = 'SMTP szerver';
+$lang['smtp_target_server'] = 'Cél Server';
+$lang['smtp_trusted_network_invalid'] = 'Megbízható hálózat érvénytelen.';
+$lang['smtp_trusted_networks'] = 'megbízható Networks';
+$lang['smtp_user_policies'] = 'felhasználói Szabályzat';
