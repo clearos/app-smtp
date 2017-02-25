1f47e8cc9b9bb12fea497f8c68c012bf27dee1b5
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:16:08 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/ht/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Yo nan lòd yo ki ap pèmèt tout karakteristik yo nan sèvè a SMTP, yo dwe sistèm nan kont dwe inisyalizèd';
+$lang['smtp_app_description'] = 'Sèvè a SMTP bay yon sèvè lapòs fèk ap rantre ak sortan osi byen ke lapòs voye ak SMTP otantifikasyon karakteristik.';
+$lang['smtp_app_name'] = 'SMTP sèvè';
+$lang['smtp_block_plaintext'] = 'Blòk plenn-tèks Modpas';
+$lang['smtp_configure_accounts'] = 'Configure Kont';
+$lang['smtp_destination_domains'] = 'destinasyon Domèn';
+$lang['smtp_failed_login_detection'] = 'SMTP Echwe Koneksyon';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail avètisman se valab.';
+$lang['smtp_mail_forwarding'] = 'mail èkspédision';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'hostname ak domèn Anviwònman yo ka configuré nan app a Mail Anviwònman.';
+$lang['smtp_maximum_message_size'] = 'Maksimòm mesaj Kantite moun ki';
+$lang['smtp_port_invalid'] = 'Port se valab.';
+$lang['smtp_recipient_mapping_invalid'] = 'kat moun k ap resevwa se valab.';
+$lang['smtp_relay_host_invalid'] = 'Relay lame se valab.';
+$lang['smtp_relay_host'] = 'Relay Akèy';
+$lang['smtp_server_invalid'] = 'SMTP sèvè se valab.';
+$lang['smtp_smtp_authentication'] = 'SMTP Otantifikasyon';
+$lang['smtp_smtp_server'] = 'SMTP sèvè';
+$lang['smtp_target_server'] = 'Sib sèvè';
+$lang['smtp_trusted_network_invalid'] = 'Ou fè konfyans rezo se valab.';
+$lang['smtp_trusted_networks'] = 'Ou fè konfyans Rezo';
+$lang['smtp_user_policies'] = 'itilizatè Règleman';
