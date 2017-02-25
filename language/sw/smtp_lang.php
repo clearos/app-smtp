c8e36a16c1491e0780dbcc900c5ead5936fae73b
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:09 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/sw/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Ili kuwezesha makala yote katika Server SMTP, mfumo lazima akaunti initialized';
+$lang['smtp_app_description'] = 'SMTP Server hutoa zinazoingia na zinazotoka mail server kama vile usambazaji barua na kujitambulisha SMTP makala.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Nywila';
+$lang['smtp_configure_accounts'] = 'Hesabu za Configure';
+$lang['smtp_destination_domains'] = 'marudio Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP Imeshindwa Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Barua disclaimer ni batili.';
+$lang['smtp_mail_forwarding'] = 'Barua Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'mazingira hostname na uwanja inaweza kimeundwa katika programu Mail vipimo.';
+$lang['smtp_maximum_message_size'] = 'Upeo Ujumbe Size';
+$lang['smtp_port_invalid'] = 'Port ni batili.';
+$lang['smtp_recipient_mapping_invalid'] = 'Mpokeaji ramani ni batili.';
+$lang['smtp_relay_host_invalid'] = 'Relay jeshi ni batili.';
+$lang['smtp_relay_host'] = 'relay Jeshi';
+$lang['smtp_server_invalid'] = 'SMTP Server ni batili.';
+$lang['smtp_smtp_authentication'] = 'SMTP Uthibitishaji';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Server Target';
+$lang['smtp_trusted_network_invalid'] = 'mtandao kuaminiwa ni batili.';
+$lang['smtp_trusted_networks'] = 'Networks kuaminiwa';
+$lang['smtp_user_policies'] = 'Sera ya mtumiaji';
