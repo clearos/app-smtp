e02113c8b4c9dc3522cd00c2c9e6f88195580a53
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:36 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lv/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Lai dotu iespēju visas funkcijas SMTP Server, grāmatojumu sistēma ir inicializēts';
+$lang['smtp_app_description'] = 'SMTP Server nodrošina ienākošo un izejošo pasta serveri, kā arī pasta pārsūtīšanas un SMTP autentifikācijas funkcijas.';
+$lang['smtp_app_name'] = 'SMTP serveris';
+$lang['smtp_block_plaintext'] = 'Block Plain teksta paroles';
+$lang['smtp_configure_accounts'] = 'Konfigurēt konti';
+$lang['smtp_destination_domains'] = 'Galamērķis Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP Neizdevās Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Pasts atruna ir nederīgs.';
+$lang['smtp_mail_forwarding'] = 'pasta nosūtīšana';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Resursdatora un domēna iestatījumus var konfigurēt pasta uzstādījumus lietotni.';
+$lang['smtp_maximum_message_size'] = 'Maksimālais Message Size';
+$lang['smtp_port_invalid'] = 'Osta ir nederīgs.';
+$lang['smtp_recipient_mapping_invalid'] = 'Saņēmējs kartēšana ir nederīga.';
+$lang['smtp_relay_host_invalid'] = 'Relay uzņēmējas ir nederīgs.';
+$lang['smtp_relay_host'] = 'relay Host';
+$lang['smtp_server_invalid'] = 'SMTP Server ir nederīgs.';
+$lang['smtp_smtp_authentication'] = 'SMTP autentifikācija';
+$lang['smtp_smtp_server'] = 'SMTP serveris';
+$lang['smtp_target_server'] = 'mērķa Server';
+$lang['smtp_trusted_network_invalid'] = 'Uzticams tīkls ir nederīgs.';
+$lang['smtp_trusted_networks'] = 'uzticami Networks';
+$lang['smtp_user_policies'] = 'Lietotāja politika';
