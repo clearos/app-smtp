e2551047d5db840bf26dadf16c7ed5a331ce1cf4
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:10 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/et/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Selleks, et kõik funktsioonid SMTP Server, kontode süsteem peab olema vormindatud';
+$lang['smtp_app_description'] = 'SMTP server annab sissetuleva ja väljamineva posti server, samuti sõnumite edastamise ja SMTP autentimine funktsioone.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Block Klaartekstiga paroolid';
+$lang['smtp_configure_accounts'] = 'seadistamine Kontod';
+$lang['smtp_destination_domains'] = 'Sihtkoht Domeenid';
+$lang['smtp_failed_login_detection'] = 'SMTP ebaõnnestus Login&#39;id';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer on kehtetu.';
+$lang['smtp_mail_forwarding'] = 'mail edastamine';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Hostinime ja domeeni saab seadistada Mail Settings app.';
+$lang['smtp_maximum_message_size'] = 'Sõnumi maksimumsuurus';
+$lang['smtp_port_invalid'] = 'Port on kehtetu.';
+$lang['smtp_recipient_mapping_invalid'] = 'Saaja kaardistamine on kehtetu.';
+$lang['smtp_relay_host_invalid'] = 'Relay peremees on kehtetu.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP server ei sobi.';
+$lang['smtp_smtp_authentication'] = 'SMTP autentimine';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Usaldusväärsed võrk ei sobi.';
+$lang['smtp_trusted_networks'] = 'Usaldusväärsed Networks';
+$lang['smtp_user_policies'] = 'Kasutaja poliitika';
