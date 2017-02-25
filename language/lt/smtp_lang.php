61f76f22d1bf1830230392608d3821e625a97e27
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:18:28 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/lt/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Kad visus SMTP Server funkcijas, dėl sąskaitų sistema turi būti inicializuoti';
+$lang['smtp_app_description'] = 'SMTP serveris pateikia gaunamus ir siunčiamus laiškus serveryje, taip pat pašto persiuntimo ir SMTP autentiškumo požymius.';
+$lang['smtp_app_name'] = 'SMTP serveris';
+$lang['smtp_block_plaintext'] = 'Blokuoti paprasto teksto slaptažodžiai';
+$lang['smtp_configure_accounts'] = 'Konfigūruoti sąskaitos';
+$lang['smtp_destination_domains'] = 'Paskirtis domenai';
+$lang['smtp_failed_login_detection'] = 'SMTP Nepavyko prisijungimai';
+$lang['smtp_mail_disclaimer_invalid'] = 'Pašto atsisakymas negalioja.';
+$lang['smtp_mail_forwarding'] = 'pašto peradresavimas';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Hostname ir domenų nustatymai gali būti konfigūruojami Mail "Nustatymų" programoje.';
+$lang['smtp_maximum_message_size'] = 'Maksimalus pranešimo dydis';
+$lang['smtp_port_invalid'] = 'Uostas yra negaliojantis.';
+$lang['smtp_recipient_mapping_invalid'] = 'Gavėjas kartografavimas yra neteisinga.';
+$lang['smtp_relay_host_invalid'] = 'Relė kompiuteris negalioja.';
+$lang['smtp_relay_host'] = 'relė Priimančioji';
+$lang['smtp_server_invalid'] = 'SMTP serveris yra neteisinga.';
+$lang['smtp_smtp_authentication'] = 'SMTP authentication';
+$lang['smtp_smtp_server'] = 'SMTP serveris';
+$lang['smtp_target_server'] = 'Tikslinė Serveris';
+$lang['smtp_trusted_network_invalid'] = 'Patikimas tinklas yra negaliojantis.';
+$lang['smtp_trusted_networks'] = 'Patikimi tinklai';
+$lang['smtp_user_policies'] = 'Vartotojo politika';
