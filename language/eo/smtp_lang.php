d8e4c487cd6899c2f810fe0f656ae9aca577506e
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:02 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/eo/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Por ebligi ĉiujn karakterizaĵojn en la SMTP servilo, la kontoj sistemo devas esti inicializado';
+$lang['smtp_app_description'] = 'La SMTP servilo disponigas envenantaj kaj eksiĝinta poŝto servilo tiel kiel poŝto forwarding kaj SMTP aŭtentikigo karakterizaĵoj.';
+$lang['smtp_app_name'] = 'SMTP-servilo';
+$lang['smtp_block_plaintext'] = 'Bloko Plain-Teksto Pasvortoj';
+$lang['smtp_configure_accounts'] = 'Agordu Kontoj';
+$lang['smtp_destination_domains'] = 'destino Domajnoj';
+$lang['smtp_failed_login_detection'] = 'SMTP Malsukcesaj logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Poŝto malgarantio estas nevalida.';
+$lang['smtp_mail_forwarding'] = 'poŝto Ekspedado';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'La gastignomon kaj havaĵo agordoj povas esti agordita en la Poŝto Fiksoj app.';
+$lang['smtp_maximum_message_size'] = 'Maksimuma Mesaĝo Grandeco';
+$lang['smtp_port_invalid'] = 'Haveno estas nevalida.';
+$lang['smtp_recipient_mapping_invalid'] = 'Ricevanto mapado estas nevalida.';
+$lang['smtp_relay_host_invalid'] = 'Relajso gastiganto estas nevalida.';
+$lang['smtp_relay_host'] = 'relajso Gastiganto';
+$lang['smtp_server_invalid'] = 'SMTP-servilo estas nevalida.';
+$lang['smtp_smtp_authentication'] = 'SMTP Authentication';
+$lang['smtp_smtp_server'] = 'SMTP-servilo';
+$lang['smtp_target_server'] = 'Cel Servilo';
+$lang['smtp_trusted_network_invalid'] = 'Fidita reto estas nevalida.';
+$lang['smtp_trusted_networks'] = 'fidita Retoj';
+$lang['smtp_user_policies'] = 'uzanto Politikoj';
