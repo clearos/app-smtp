7245b6a51bab2066cd9ec22faea264e45f02617a
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:19:58 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/nl/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Om alle functies van de SMTP-server in te schakelen, moet het systeem rekeningen worden geïnitialiseerd';
+$lang['smtp_app_description'] = 'De SMTP-server biedt een inkomende en uitgaande mailserver als mail forwarding en SMTP-verificatie functies.';
+$lang['smtp_app_name'] = 'SMTP-server';
+$lang['smtp_block_plaintext'] = 'Block Plain Text-wachtwoorden';
+$lang['smtp_configure_accounts'] = 'Accounts configureren';
+$lang['smtp_destination_domains'] = 'bestemming Domeinen';
+$lang['smtp_failed_login_detection'] = 'SMTP mislukt Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail disclaimer is ongeldig.';
+$lang['smtp_mail_forwarding'] = 'mail Forwarding';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'De hostnaam en domein instellingen kunnen in de app Mail instellingen worden geconfigureerd.';
+$lang['smtp_maximum_message_size'] = 'Maximum Message Size';
+$lang['smtp_port_invalid'] = 'Poort is ongeldig.';
+$lang['smtp_recipient_mapping_invalid'] = 'Ontvanger mapping is ongeldig.';
+$lang['smtp_relay_host_invalid'] = 'Relayhost is ongeldig.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'SMTP-server is ongeldig.';
+$lang['smtp_smtp_authentication'] = 'SMTP-verificatie';
+$lang['smtp_smtp_server'] = 'SMTP-server';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'Vertrouwd netwerk is ongeldig.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'gebruikersbeleid';
