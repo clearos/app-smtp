33b96325b826886348931311a1b38212df26d98c
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:15:43 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/de/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Um alle Funktionen in der SMTP-Server zu aktivieren, müssen die Konten System initialisiert werden';
+$lang['smtp_app_description'] = 'Der SMTP-Server stellt einen eingehenden und ausgehenden Mail-Server sowie E-Mail-Weiterleitung und die SMTP-Authentifizierung Funktionen.';
+$lang['smtp_app_name'] = 'SMTP-Server';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Passwörter';
+$lang['smtp_configure_accounts'] = 'Accounts konfigurieren';
+$lang['smtp_destination_domains'] = 'Destination Domains';
+$lang['smtp_failed_login_detection'] = 'SMTP-Fehl-Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail Disclaimer ist ungültig.';
+$lang['smtp_mail_forwarding'] = 'Mail-Weiterleitung';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Die Hostnamen und Domain-Einstellungen können in der Mail-App Einstellungen konfiguriert werden.';
+$lang['smtp_maximum_message_size'] = 'Maximale Nachrichtengröße';
+$lang['smtp_port_invalid'] = 'Port ist ungültig.';
+$lang['smtp_recipient_mapping_invalid'] = 'Empfänger-Mapping ist ungültig.';
+$lang['smtp_relay_host_invalid'] = 'Relay Host ist ungültig.';
+$lang['smtp_relay_host'] = 'Relay-Host';
+$lang['smtp_server_invalid'] = 'SMTP-Server ist ungültig.';
+$lang['smtp_smtp_authentication'] = 'SMTP-Authentifizierung';
+$lang['smtp_smtp_server'] = 'SMTP-Server';
+$lang['smtp_target_server'] = 'Zielserver';
+$lang['smtp_trusted_network_invalid'] = 'Trusted Netzwerk ist ungültig.';
+$lang['smtp_trusted_networks'] = 'Trusted Networks';
+$lang['smtp_user_policies'] = 'Benutzerrichtlinien';
