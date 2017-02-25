c71eb56fc86ef26ea9ddb2444191ca1c054ff660
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:24:16 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/zu/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Ukuze unike amandla zonke izici Server SMTP, uhlelo ama-akhawunti kumele kuqaliswe';
+$lang['smtp_app_description'] = 'Iseva ye-SMTP kunikeza mail iseva namakholi angenayo naphumayo kanye mail ukudlulisa kanye SMTP ukufakazela ubuqiniso izici.';
+$lang['smtp_app_name'] = 'Iseva ye-SMTP';
+$lang['smtp_block_plaintext'] = 'Block Plain-Text Amaphasiwedi';
+$lang['smtp_configure_accounts'] = 'Lungiselela Accounts';
+$lang['smtp_destination_domains'] = 'Domains Destination';
+$lang['smtp_failed_login_detection'] = 'SMTP Yehlulekile ungene';
+$lang['smtp_mail_disclaimer_invalid'] = 'Mail nesinqindi ayivumelekile.';
+$lang['smtp_mail_forwarding'] = 'Mail Ukudlulisela';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Igama lomethuleli domain izilungiselelo kungenziwa amisiwe in lokusebenza izilungiselelo Mail.';
+$lang['smtp_maximum_message_size'] = 'Maximum Usayizi Umlayezo';
+$lang['smtp_port_invalid'] = 'Port ayivumelekile.';
+$lang['smtp_recipient_mapping_invalid'] = 'Umamukeli mapping ayivumelekile.';
+$lang['smtp_relay_host_invalid'] = 'Edluliselwe host ayivumelekile.';
+$lang['smtp_relay_host'] = 'Relay Host';
+$lang['smtp_server_invalid'] = 'Iseva ye-SMTP ayivumelekile.';
+$lang['smtp_smtp_authentication'] = 'SMTP Ukufakazela ubuqiniso';
+$lang['smtp_smtp_server'] = 'Iseva ye-SMTP';
+$lang['smtp_target_server'] = 'Target Server';
+$lang['smtp_trusted_network_invalid'] = 'inethiwekhi Othenjwayo ayivumelekile.';
+$lang['smtp_trusted_networks'] = 'Amanethiwekhi Othenjwayo';
+$lang['smtp_user_policies'] = 'Izinqubomgomo Umsebenzisi';
