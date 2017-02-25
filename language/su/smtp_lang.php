1e7f095ac38c98bb0bee6e299e0e5153b88827f0
Apache <apache@ip-172-31-15-54.us-west-2.compute.internal>
2017-02-25 17:22:01 +0000

    Add new files and directories and commit changes to local repository

---
+++ language/su/smtp_lang.php
@@ -0,0 +1,24 @@
+<?php
+
+$lang['smtp_accounts_system_not_initialized_warning'] = 'Dina raraga pikeun ngaktipkeun sadaya fitur dina SMTP Server, sistim akun kudu initialized';
+$lang['smtp_app_description'] = 'Nu SMTP Server nyadiakeun hiji server mail asup jeung telepon kaluar ogé mail diteruskeun jeung auténtikasi SMTP fitur.';
+$lang['smtp_app_name'] = 'SMTP Server';
+$lang['smtp_block_plaintext'] = 'Blok Userboxtop-Téks kecap akses';
+$lang['smtp_configure_accounts'] = 'Akun Konpigurasikeun';
+$lang['smtp_destination_domains'] = 'domain tujuan';
+$lang['smtp_failed_login_detection'] = 'SMTP Gagal Logins';
+$lang['smtp_mail_disclaimer_invalid'] = 'Surat Bantahan téh sah.';
+$lang['smtp_mail_forwarding'] = 'surat Neraskeun';
+$lang['smtp_mail_settings_in_mail_app_message'] = 'Nu hostname jeung domain setélan bisa ngonpigurasi di app surat Setelan.';
+$lang['smtp_maximum_message_size'] = 'Maksimum Ukuran Message';
+$lang['smtp_port_invalid'] = 'Sindangsari nyaéta sah.';
+$lang['smtp_recipient_mapping_invalid'] = 'pemetaan panarima anu sah.';
+$lang['smtp_relay_host_invalid'] = 'Relay host anu sah.';
+$lang['smtp_relay_host'] = 'relay inangna';
+$lang['smtp_server_invalid'] = 'SMTP Server nyaéta sah.';
+$lang['smtp_smtp_authentication'] = 'SMTP auténtikasi';
+$lang['smtp_smtp_server'] = 'SMTP Server';
+$lang['smtp_target_server'] = 'target Server';
+$lang['smtp_trusted_network_invalid'] = 'jaringan dipercaya téh sah.';
+$lang['smtp_trusted_networks'] = 'Jaringan nu dipercaya';
+$lang['smtp_user_policies'] = 'Kawijakan pamaké';
